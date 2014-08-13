cf8392f0989b28d7e78ed032bb917902d641d33c
rgupta <rgupta@35fd5345-5407-9947-aa4f-5170b9b8abad>
2014-08-13 10:25:11 +0000

    minor cleanup
    git-svn-id: https://192.168.2.203:8080/svn/RWrappers/trunk@3 35fd5345-5407-9947-aa4f-5170b9b8abad

--- FLKMeans_with_DYNAMIC_RESULT_SETS.r
+++ FLKMeans_with_DYNAMIC_RESULT_SETS.r
@@ -1,53 +1,53 @@
-notNaturalNumber <- function(x)
+is.naturalNumber <- function(x)
 {
-	if (class(x) != "numeric") 
-        return(TRUE);
+	if (!is.numeric(x)) 
+        return(FALSE);
 		
     if (ceiling(x) != floor(x)) 
-        return(TRUE);
+        return(FALSE);
 		
 	if (x<=0) 
-        return(TRUE);
+        return(FALSE);
 		
-	return(FALSE);
+	return(TRUE);
 }
 
 FLKMeans <- function(TableName, ObsIDColName, VarIDColName, ValueColName, WhereClause, Clusters, Iterations, Hypothesis, Note)
 {
 	#check TableName
-    if (class(TableName) != "character") 
+    if (!is.character(TableName)) 
         return("First parameter is the table name and it should be a string");
 	
 	#check ObsIDColName
-    if (class(ObsIDColName) != "character") 
-        return("Second parameter is the table name and it should be a string");
+    if (!is.character(ObsIDColName)) 
+        return("Second parameter is the name of the observation ID column and it should be a string");
 		
 	#check VarIDColName
-    if (class(VarIDColName) != "character") 
-        return("Third parameter is the table name and it should be a string");
+    if (!is.character(VarIDColName)) 
+        return("Third parameter is the name of the Variable ID column and it should be a string");
 		
 	#check ValueColName
-    if (class(ValueColName) != "character") 
-        return("Fourth parameter is the table name and it should be a string");
+    if (!is.character(ValueColName)) 
+        return("Fourth parameter is the name of the Value Column and it should be a string");
 		
 	#check WhereClause
-    if (class(WhereClause) != "character") 
+    if (!is.character(WhereClause)) 
         return("Fifth parameter is an Additional filter for restricting input data and it should be a string");
 		
     #check notes
-    if (class(Note) != "character") 
+    if (!is.character(Note)) 
         return("Ninth parameter is the user defined comment and it should be a string");
 	
 	#check Clusters 
-    if (notNaturalNumber(Clusters)) 
+    if (!is.naturalNumber(Clusters)) 
         return("Seventh parameter is Number of clusters and it should be an integer greater than zero");
 		
 	#check Iterations 
-    if (notNaturalNumber(Iterations)) 
+    if (!is.naturalNumber(Iterations)) 
         return("Seventh parameter is maximum iterations and it should be an integer greater than zero");
 		
 	#check Hypothesis 
-    if (notNaturalNumber(Hypothesis)) 
+    if (!is.naturalNumber(Hypothesis)) 
         return("Eighth parameter is Number of Hypotheses and it should be an integer greater than zero");
 		
 	#Generate SQL
