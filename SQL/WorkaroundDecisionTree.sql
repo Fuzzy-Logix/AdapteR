0d787a36950e16e0cff75c5b832d8c37c83606c3
mmundra <mmundra@35fd5345-5407-9947-aa4f-5170b9b8abad>
2014-09-02 12:33:23 +0000

    Stored procedure for decision tree
    git-svn-id: https://192.168.2.203:8080/svn/RWrappers/trunk@11 35fd5345-5407-9947-aa4f-5170b9b8abad

---
+++ SQL/WorkaroundDecisionTree.sql
@@ -0,0 +1,20 @@
+REPLACE PROCEDURE WorkaroundDecisionTree (	IN TableName VARCHAR(100), 
+											IN ObsIDColName VARCHAR(100),
+											IN VarIDColName VARCHAR(100),
+											IN ValueColName VARCHAR(100),
+											IN NumOfSplits INTEGER,
+											IN MaxLevel INTEGER,
+											IN PurityThreshold DOUBLE PRECISION,								
+											IN Note VARCHAR(255) )
+DYNAMIC RESULT SETS 1
+BEGIN
+	DECLARE AnalysisID VARCHAR(30);
+	DECLARE statement1_str VARCHAR(500);
+	DECLARE result_set CURSOR WITH RETURN ONLY FOR stmt1;
+	
+	CALL FLDecisionTree(TableName,ObsIDColName,VarIDColName,ValueColName,NumOfSplits,MaxLevel,PurityThreshld,Note,AnalysisID);    
+	SET statement1_str = 'SELECT Analysisid FROM fzzlDecisionTreeInfo WHERE Analysisid = ?';
+	PREPARE stmt1 FROM statement1_str;
+	OPEN result_set USING AnalysisID;
+	DEALLOCATE PREPARE stmt1;
+END;
