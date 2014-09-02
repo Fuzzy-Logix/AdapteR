12462b5af1d51e5f66cd276e39362f349415055c
rgupta <rgupta@35fd5345-5407-9947-aa4f-5170b9b8abad>
2014-09-02 09:49:10 +0000

    Minor changes/fixes during Demo
    git-svn-id: https://192.168.2.203:8080/svn/RWrappers/trunk@9 35fd5345-5407-9947-aa4f-5170b9b8abad

--- RScripts/BarebonesRKMeans.r
+++ RScripts/BarebonesRKMeans.r
@@ -1,7 +1,9 @@
-# Barebones R KMeans
-
+require(graphics)
 
 # a 2-dimensional example
-Data <- rbind(matrix(rnorm(100, sd = 0.3), ncol = 2),
+x <- rbind(matrix(rnorm(100, sd = 0.3), ncol = 2),
            matrix(rnorm(100, mean = 1, sd = 0.3), ncol = 2))
-res <- kmeans(x, 5, iter.max=30, nstart = 25)
+colnames(x) <- c("x", "y")
+(cl <- kmeans(x, 2))
+plot(x, col = cl$cluster)
+points(cl$centers, col = 1:2, pch = 8, cex = 2)
--- RScripts/FLDemo.r
+++ RScripts/FLDemo.r
@@ -8,6 +8,7 @@ setwd("C:/Users/STPL/Fuzzy Logix/RWrappers/trunk/S3")
 source("S3FLTable.r")
 source("S3FLkmeans.r")
 library(RODBC)
+require(graphics)
 
 #Prep Demo
 
@@ -31,4 +32,6 @@ merged <- merge(res3$ClusterID,allpoints,by.x="ObsID",by.y="ID")
 #centers <- matrix(c(res3$Dendrogram[res3$Dendrogram$VarID == 1,"Centroid"],res3$Dendrogram[res3$Dendrogram$VarID == 2,"Centroid"]),ncol=2,dimnames = list(c(),c("x","y")))
 
 plot(merged$X,merged$Y,col = merged$ClusterID)
-points(res3$Dendrogram[res3$Dendrogram$VarID == 1,"Centroid"],res3$Dendrogram[res3$Dendrogram$VarID == 2,"Centroid"], col = 1:2, pch = 8, cex = 2)
+centers <- list(x=res3$Dendrogram[res3$Dendrogram$VarID == 1,"Centroid"],
+				y=res3$Dendrogram[res3$Dendrogram$VarID == 2,"Centroid"])
+points(centers[["x"]],centers[["y"]], col = 1:2, pch = 8, cex = 2)
