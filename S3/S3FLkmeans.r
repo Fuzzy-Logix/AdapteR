b23492607848e7676a2676525fff8ecc2c27a896
rgupta <rgupta@35fd5345-5407-9947-aa4f-5170b9b8abad>
2014-09-15 12:49:30 +0000

    Readability changes to S3FLkmeans.r
    git-svn-id: https://192.168.2.203:8080/svn/RWrappers/trunk@16 35fd5345-5407-9947-aa4f-5170b9b8abad

--- S3/S3FLkmeans.r
+++ S3/S3FLkmeans.r
@@ -1,34 +1,37 @@
-FLkmeans <- function( x,
-                      centers,
-                      iter.max = 10, 
-                      nstart = 1,
-                      Note = "From RWrapper For DBLytix"){
+FLkmeans <- function( 	x,
+                  		centers,
+						iter.max = 10, 
+						nstart   = 1,
+						Note     = "From RWrapper For DBLytix"){
 
-	ObsIDColName <- "ObsID";
-	VarIDColName <- "VarID";
-	ValueColName <- "Num_Val";
-	WhereClause <- "";
-	DBConnection <- x[["connection"]];
-	SQLStr <- paste(	"CALL WorkaroundKMeans('", 
-						x[["DeepTableName"]], "',", "'",
-						ObsIDColName, "',", "'", 
-						VarIDColName, "',", "'", 
-						ValueColName, "','", 
-						WhereClause, "','", 
-						toString(centers), "','", 
-						toString(iter.max),"','", 
-						toString(nstart),"',", "'", 
-						Note,"')", sep="")
-
-	#run KMeans
-    KMeansRes <- sqlQuery(DBConnection, SQLStr);
-	AnalysisID <- toString(KMeansRes$ANALYSISID);
+	ObsIDColName  <- "ObsID";
+	VarIDColName  <- "VarID";
+	ValueColName  <- "Num_Val";
+	WhereClause   <- "";
+	DBConnection  <- x[["connection"]];
+	SQLStr        <- "CALL WorkaroundKMeans('";
+	SQLParameters <- paste(	x[["DeepTableName"]],
+							ObsIDColName,  
+							VarIDColName, 
+							ValueColName, 
+							WhereClause, 
+							toString(centers),  
+							toString(iter.max), 
+							toString(nstart),
+							Note, sep="','")
+	SQLStr           <- paste(SQLStr, SQLParameters,"')", sep="")
 	
-	SQLStr <- paste("SELECT HypothesisID,Level,ClusterID,VarID,Centroid FROM fzzlKMeansDendrogram WHERE AnalysisID = '", AnalysisID,"' ORDER BY 1,2,3,4",sep = "");
+	#run KMeans
+	KMeansRes        <- sqlQuery(DBConnection, SQLStr);
+	AnalysisID       <- toString(KMeansRes$ANALYSISID);
+	
+	#Fetch Centers Dendrogram
+	SQLStr           <- paste("SELECT HypothesisID,Level,ClusterID,VarID,Centroid FROM fzzlKMeansDendrogram WHERE AnalysisID = '", AnalysisID,"' ORDER BY 1,2,3,4",sep = "");
 	KMeansDendrogram <- sqlQuery(DBConnection, SQLStr);
 	
-	SQLStr <- paste("SELECT HypothesisID,ObsID,ClusterID FROM fzzlKMeansClusterID WHERE AnalysisID = '", AnalysisID,"' ORDER BY 1,2,3",sep = "");
-	KMeansClusterID <- sqlQuery(DBConnection, SQLStr);
+	#Fetch ClusterID Arrays
+	SQLStr           <- paste("SELECT HypothesisID,ObsID,ClusterID FROM fzzlKMeansClusterID WHERE AnalysisID = '", AnalysisID,"' ORDER BY 1,2,3",sep = "");
+	KMeansClusterID  <- sqlQuery(DBConnection, SQLStr);
 
 	RetData = list(AnalysisID = AnalysisID, Dendrogram = KMeansDendrogram, ClusterID = KMeansClusterID);
 	
