34e00fa86ddac35aeed0c34d803373b26e5dd53d
rgupta <rgupta@35fd5345-5407-9947-aa4f-5170b9b8abad>
2014-09-16 18:02:00 +0000

    Added S4FLTable.r
    git-svn-id: https://192.168.2.203:8080/svn/RWrappers/trunk@18 35fd5345-5407-9947-aa4f-5170b9b8abad

--- S4/S4FLTable.r
+++ S4/S4FLTable.r
@@ -5,4 +5,22 @@
 # @param  {character} TableName 		  [description]
 #
  
-setClass("FLTable",)
+setClass("FLTable", 
+		slots = list(	ODBCConnection = "integer", 
+						DBName        = "character", 
+						TableName     = "character"))
+
+FLTable <- function(Connection,DBName,TableName) {
+
+	#if (!is.character(DSN)) 		
+	#stop("DSN must be a string")
+	if (!is.character(DBName)) 		
+	stop("DBName must be a string")
+	if (!is.character(TableName))	
+	stop("TableNamemust be a string")
+
+	sqlQuery(Connection, "SET ROLE ALL");
+	sqlQuery(Connection, paste("DATABASE", DBName));
+
+	new("FLTable", ODBCConnection = Connection,DBName = DBName, TableName = TableName)
+}
