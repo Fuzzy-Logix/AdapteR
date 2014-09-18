8f002f412cbd8da1095d932393790d42792f03e0
mmundra <mmundra@35fd5345-5407-9947-aa4f-5170b9b8abad>
2014-09-18 19:39:39 +0000

    git-svn-id: https://192.168.2.203:8080/svn/RWrappers/trunk@23 35fd5345-5407-9947-aa4f-5170b9b8abad

--- S4/results.r
+++ S4/results.r
@@ -1,13 +1,17 @@
 setOldClass("RODBC");
+
+# define fetch.results Generic
+setGeneric("fetch.results", function(object) {
+  standardGeneric("fetch.results")
+})
+
+# define FLKMeans Class
 setClass("FLKMeans", 
 		slots = list(	ODBCConnection = "RODBC",
 						AnalysisID     = "character", 
 						centers        = "data.frame", 
 						cluster        = "data.frame"))
 
-setGeneric("fetch.results", function(object) {
-  standardGeneric("fetch.results")
-})
 
 # fetch_results method for KMeans
 setMethod("fetch.results",
@@ -24,6 +28,26 @@ setMethod("fetch.results",
 
 			object@centers = KMeansDendrogram;
 			object@cluster = KMeansClusterID;
+			#print(paste(object@AnalysisID));
+			object
+          }
+)
+# define FLDecisionTree Class
+setClass("FLDecisionTree", 
+		slots = list(	ODBCConnection = "RODBC",
+						AnalysisID     = "character", 
+						decision_tree  = "data.frame"))
+						
+# fetch_results method for FLDecisionTree
+setMethod("fetch.results",
+          signature("FLDecisionTree"),
+          function(object) {
+      DBConnection <- object@ODBCConnection;            
+      #Fetch Decision Tree Analysis Result Table
+			SQLStr <- paste("SELECT NodeID,SplitVarID,SplitVal,ParentNodeID,ChildType,Lvl,IsLeaf,ClassValue,NodeSize,EventRate FROM fzzlDecisionTree WHERE AnalysisID = '", object@AnalysisID,"' ORDER BY 1,2,3,4",sep = "");
+			FinalResult <- sqlQuery(DBConnection, SQLStr);
+			object@decision_tree = FinalResult;
+			
 			#print(paste(object@AnalysisID));
 			object
           }
