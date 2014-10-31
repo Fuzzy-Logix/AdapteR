bcba5f3f17d7df558196a1739837fa663c887c1f
mmundra <mmundra@35fd5345-5407-9947-aa4f-5170b9b8abad>
2014-10-31 12:13:44 +0000

    git-svn-id: https://192.168.2.203:8080/svn/RWrappers/trunk@59 35fd5345-5407-9947-aa4f-5170b9b8abad

---
+++ RScripts/UdtDemo.r
@@ -0,0 +1,21 @@
+rm(list=ls())
+setwd("D:/R wrappers/RWrappers/trunk")
+
+# include
+source("S4//FLMatrix.r")
+source("S4//utilities.r")
+source("S4//results.r")
+
+library(RODBC)
+DBConnect <- odbcConnect("Gandalf")
+
+########################################################################################################
+
+# Demo for FLCholeskyDecomp
+source("S4//FLCholeskyDecomp.r")
+# Create FLMatrix object
+InMatrix <-  FLMatrix(DBConnect, DBName = "FL_R_WRAP", MatrixTableName = "tblMatrixMulti", MatrixID = 5)
+# Perform Cholesky Decomposition
+OutMatrix <- FLCholeskyDecomp(InMatrix)
+
+########################################################################################################
