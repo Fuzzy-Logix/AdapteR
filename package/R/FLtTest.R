8e16ef5ec7caea600c71f2ca91e483b2d8a49849
rgupta <rgupta@35fd5345-5407-9947-aa4f-5170b9b8abad>
2014-12-05 14:38:49 +0000

    Adding fixed hypothesis tests from local dir
    git-svn-id: https://192.168.2.203:8080/svn/RWrappers/trunk@95 35fd5345-5407-9947-aa4f-5170b9b8abad

---
+++ package/R/FLtTest.R
@@ -0,0 +1,79 @@
+#' Student's t-Test
+#' 
+#' Performs one and two sample z-tests on vectors of data. 
+#
+#' @details One-sample Z-test is used to test if population mean of the sample is 
+#' significantly different from a given reference value.
+#' Two-sample Z-test is used to determine if the population means from the two
+#' samples are equal.
+#' 
+#' @param table An object of class \code{FLTable}
+#' @param primary_key name of primary key column of the table mapped to FLTable
+#' @param input1 column name for first sample input vector
+#' @param input2 (only in case of two-sample test) column name for second sample input vector
+#' @param mu (only in case of one-sample test) reference mean
+#' @param var.equal (only in case of two-sample test) a logical variable indicating whether to treat the two variances as being equal
+#' @param num_tails number of tails in test distribution. This decides whether
+#' one-tailed test or two-tailed test is performed \cr Ref: \url{http://www.ats.ucla.edu/stat/mult_pkg/faq/general/tail_tests.htm}
+#'
+#' @return returns the value of t-test statistic and probability (P-Value) of test statistic
+#'
+#' If the input vector contains zero non-null observations, the function returns a NULL. Also, if the standard 
+#' deviation of values in the input vector is zero, the function returns a NULL value.
+#' 
+#' @examples
+#' \dontrun{
+#' 
+#' FLtTest(table = tbl, primary_key = "ObsID", input1 = "Var1", mu = 0.45, num_tails = 2)
+#' FLtTest(table = tbl, primary_key = "ObsID", input1 = "Var1", input2 = "Var2",var.equal = FALSE, num_tails = 2)
+#'
+#' }
+#'
+#'@export
+FLtTest <- function(	table,
+						primary_key = "ObsID", 
+						input1, 
+						input2 = NULL, 
+						mu = 0, 
+						var.equal = FALSE, 
+						num_tails = 2 ) 
+{
+	if(length(input2) == 0) 
+	{
+		#One Sample t-Test
+		connection    <- table@odbc_connection
+		path          <- "FLtTest1S.sql"
+		sqlParameters <- list( 	tableName = table@table_name,
+								input1    = input1,
+								mu        = mu,
+								numTails  = num_tails)
+		res <- run_sql(connection, path, sqlParameters)
+		return(res)
+	}
+	else {
+		#Two Sample t-Test
+		
+		connection    <- table@odbc_connection		
+		obsIDColName  <- "ObsID"
+		varIDColName  <- "GroupID"
+		valueColName  <- "Num_Val"
+		whereClause   <- ""
+		deepTableName <- wide_to_deep(	table,
+										primary_key  = primary_key,
+										obs_id       = obsIDColName,
+										var_id       = varIDColName,
+										value        = valueColName,
+										exclude      = c(),
+										class_spec   = list(),
+										where_clause = whereClause)
+		ifelse(var.equal == FALSE, varType <- "UNEQUAL_VAR", varType <- "EQUAL_VAR")
+		# Two Sample t-Test
+		path <- "FLtTest2S.sql"
+		sqlParameters <- list( 	tableName = deepTableName,
+								varType   = varType,
+								numTails  = num_tails)	
+		res <- run_sql(connection, path, sqlParameters)
+		sqlQuery(connection,paste("DROP TABLE",deepTableName))
+		return(res)
+	}
+}
