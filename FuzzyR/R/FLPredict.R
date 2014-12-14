c06bb2f8cb57365266a46025f07b8e84cc3e11db
mmundra <mmundra@35fd5345-5407-9947-aa4f-5170b9b8abad>
2014-12-14 13:19:29 +0000

    git-svn-id: https://192.168.2.203:8080/svn/RWrappers/trunk@126 35fd5345-5407-9947-aa4f-5170b9b8abad

--- FuzzyR/R/FLPredict.R
+++ FuzzyR/R/FLPredict.R
@@ -7,14 +7,16 @@ NULL
 #' Scoring function
 #'
 #' \code{FLPredict} scores a new dataset using an existing data mining analysis
-#' model. 
+#' model.
 #'
 #' @details \code{FLPredict} predicts the value of the dependent variable using
 #' an existing model for the analysis objects of the following classes.
-#' \item{FLLinRegr}{Linear Regression}
-#' \item{FLLogRegr}{Logistic Regression}
-#' \item{FLNaiveBayes}{Naive Bayes}
-#' \item{FLDecisionTree}{Decision Tree}
+#' \itemize{
+#' \item{FLLinRegr}
+#' \item{FLLogRegr}
+#' \item{FLNaiveBayes}
+#' \item{FLDecisionTree}
+#' }
 #'
 #' @param analysis a fitted model object of the relevant type with the parameters
 #' filled in. The predict function currently scores the dataset for the analysis
@@ -22,36 +24,36 @@ NULL
 #' @param table an \code{FLTable} object mapped to the dataset which is to be
 #' scored
 
-#' @return a \code{data.frame} having the scored value of the dependent variable
+#' @return \code{FLPredict} returns a \code{data.frame} having the scored value of the dependent variable
 #'
 #' @examples
 #' \dontrun{
-#'		
+#'
 #' }
 #'
 #'@export
-setGeneric("FLPredict", function(	analysis, 
-									table, 
+setGeneric("FLPredict", function(	analysis,
+									table,
 									... ) {
   standardGeneric("FLPredict")
 })
 
 # score function for FLLinRegr
-setMethod(	"FLPredict", 
+setMethod(	"FLPredict",
 			signature(	analysis = "FLLinRegr",
 						table = "FLTable"),
-			function(	analysis, 
-						table)  
+			function(	analysis,
+						table)
 			{
 				obsID 			<- "ObsID"
 				varID 			<- "VarID"
 				value 			<- "Num_Val"
 				primaryKey   	<- analysis@primary_key
 				exclude      	<- analysis@exclude
-				classSpec    	<- analysis@class_spec	
+				classSpec    	<- analysis@class_spec
 				inAnalysisID 	<- analysis@wide_to_deep_analysis_id
 				whereClause  	<- ""
-				
+
 				#Convert wide table to deep format
 				dataPrepRes 	<- regr_data_prep_score( 	table,
 															obs_id 			= obsID,
@@ -65,20 +67,20 @@ setMethod(	"FLPredict",
 				deepTableName      		<- dataPrepRes$deepTableName
 				wideToDeepAnalysisID 	<- dataPrepRes$wideToDeepAnalysisID
 				regrAnalysisID 		 	<- analysis@analysis_id
-				connection         		<- table@odbc_connection				
-				
+				connection         		<- table@odbc_connection
+
 				sqlParameters <- list(  deepTableName  = deepTableName,
 										obsID          = obsID,
 										varID          = varID,
 										value          = value,
 										whereClause    = whereClause,
 										regrAnalysisID = regrAnalysisID)
-				
+
 				#run LinRegrScore
 				linRegrScoreRes <- run_sql(connection, "FLLinRegrScore.sql", sqlParameters)
 				outTableName    <- toString(linRegrScoreRes[[1]])
-				
-				#Fetch 
+
+				#Fetch
 				sqlParameters   <- list(  outTableName  = outTableName )
 				scoreTable      <- run_sql(connection, "FLLinRegrScoreFetch.sql", sqlParameters)
 				return(scoreTable)
@@ -86,21 +88,21 @@ setMethod(	"FLPredict",
 		)
 
 # score function for FLLogRegr
-setMethod(	"FLPredict", 
-			signature(	analysis = "FLLogRegr", 
+setMethod(	"FLPredict",
+			signature(	analysis = "FLLogRegr",
 						table = "FLTable"),
-			function(	analysis, 
-						table)  
+			function(	analysis,
+						table)
 			{
 				obsID 		 	<- "ObsID"
 				varID 		 	<- "VarID"
 				value 		 	<- "Num_Val"
 				primaryKey   	<- analysis@primary_key
 				exclude      	<- analysis@exclude
-				classSpec    	<- analysis@class_spec	
+				classSpec    	<- analysis@class_spec
 				inAnalysisID 	<- analysis@wide_to_deep_analysis_id
 				whereClause  	<- ""
-				
+
 				#Convert wide table to deep format
 				dataPrepRes 	<- regr_data_prep_score( 	table,
 															obs_id 			= obsID,
@@ -110,7 +112,7 @@ setMethod(	"FLPredict",
 															exclude     	= exclude,
 															class_spec  	= classSpec,
 															where_clause	= whereClause,
-															in_analysis_id 	= inAnalysisID)					
+															in_analysis_id 	= inAnalysisID)
 				deepTableName        	<- dataPrepRes$deepTableName
 				wideToDeepAnalysisID 	<- dataPrepRes$wideToDeepAnalysisID
 				regrAnalysisID 		 	<- analysis@analysis_id
@@ -122,36 +124,36 @@ setMethod(	"FLPredict",
 										value          = value,
 										whereClause    = whereClause,
 										regrAnalysisID = regrAnalysisID)
-				
+
 				#run LinRegrScore
 				logRegrScoreRes <- run_sql(connection, "FLLogRegrScore.sql", sqlParameters)
 				outTableName    <- toString(logRegrScoreRes[[1]])
-				
-				#Fetch 
+
+				#Fetch
 				sqlParameters   <- list(  outTableName  = outTableName )
 				scoreTable      <- run_sql(connection, "FLLinRegrScoreFetch.sql", sqlParameters)
 				return(scoreTable)
-				
+
 			}
 		)
 
 #score function for FLNaiveBayes
-setMethod(	"FLPredict", 
-			signature(	analysis = "FLNaiveBayes", 
+setMethod(	"FLPredict",
+			signature(	analysis = "FLNaiveBayes",
 						table = "FLTable"),
-			function(	analysis, 
-						table, 
+			function(	analysis,
+						table,
 						predict_table = "")
-			{							
+			{
 				obsID 			<- "ObsID"
 				varID 			<- "VarID"
 				value 			<- "Num_Val"
 				primaryKey   	<- analysis@primary_key
 				exclude      	<- analysis@exclude
-				classSpec    	<- analysis@class_spec	
+				classSpec    	<- analysis@class_spec
 				inAnalysisID 	<- analysis@wide_to_deep_analysis_id
 				whereClause  	<- ""
-				
+
 				#Convert wide table to deep format
 				dataPrepRes  	<- regr_data_prep_score( 	table,
 															obs_id 			= obsID,
@@ -166,13 +168,13 @@ setMethod(	"FLPredict",
 				wideToDeepAnalysisID 	<- dataPrepRes$wideToDeepAnalysisID
 				regrAnalysisID       	<- analysis@analysis_id
 				connection         		<- table@odbc_connection
-				
+
 				#Generate name of the output table
 				if(predict_table == "")
 				{
-					predict_table 		<- gen_out_table("NaiveBayes",regrAnalysisID)	
-				}												
-				
+					predict_table 		<- gen_out_table("NaiveBayes",regrAnalysisID)
+				}
+
 				sqlParameters <- list(  deepTableName  = deepTableName,
 										obsID          = obsID,
 										varID          = varID,
