ae3ebc015a6048e0b33a87d08154b462b3e50977
suryavamshig <suryavamshig@35fd5345-5407-9947-aa4f-5170b9b8abad>
2015-05-28 07:38:46 +0000

    Added a names function for FLTable objects, similar to a data.frame.
    git-svn-id: https://192.168.2.203:8080/svn/RWrappers/trunk@142 35fd5345-5407-9947-aa4f-5170b9b8abad

--- Project_RNagda/FLKMeans.R
+++ Project_RNagda/FLKMeans.R
@@ -14,17 +14,29 @@ gen_deep_table_name <- function(TableName){
 	random_no <- rnorm(1);
 	paste(TableName,"Deep",round(as.numeric(Sys.time())),round(random_no*random_no*10000000),sep="_");
 }
+trim <- function( x ) {
+  gsub("(^[[:space:]]+|[[:space:]]+$)", "", x)
+}
 
-FLTable <- function(connection, database, table,primary_key=NULL) {
+FLTable <- function(connection, database, table,primary_key=character(0)) {
 
 	# validate_args( 	list(database = database, table = table), 
 	# 				list(database = "character", table = "character")
 	# )
-
 	sqlQuery(connection, paste("DATABASE", database))
 	sqlQuery(connection, "SET ROLE ALL")
 
 	new("FLTable", odbc_connection = connection,db_name = database, table_name = table,primary_key = primary_key)
+}
+
+names.FLTable <- function(object){
+		connection = object@odbc_connection
+		column_database = "dbc"
+		sqlQuery(connection, paste("DATABASE", column_database))
+		sqlQuery(connection, "SET ROLE ALL")
+		sqlstr = paste0("SELECT columnname FROM dbc.columns WHERE tablename='",object@table_name,"' AND databasename='",object@db_name,"';")
+		retobj = sqlQuery(connection,sqlstr)
+		retobj<-trim(as.vector(retobj$ColumnName))
 }
 
 setClass(
