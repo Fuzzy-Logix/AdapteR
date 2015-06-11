63f44275b2ee332252edd1bd2b189b50bb9f6cf2
suryavamshig <suryavamshig@35fd5345-5407-9947-aa4f-5170b9b8abad>
2015-06-11 13:27:13 +0000

    Overloaded solve() for FLMatrix objects
    git-svn-id: https://192.168.2.203:8080/svn/RWrappers/trunk@148 35fd5345-5407-9947-aa4f-5170b9b8abad

--- Project_RNagda/FLMatrixDetUdt.R
+++ Project_RNagda/FLMatrixDetUdt.R
@@ -29,12 +29,31 @@ det <- function (x, ...){
 }
 
 det.FLMatrix<-function(object){
-	connection <- object@odbc_connection
-	sqlstr <- paste0("WITH z (Matrix_ID, Row_ID, Col_ID, Cell_Val) AS (SELECT a.",object@matrix_id_colname,", a.",object@row_id_colname,", a.",object@col_id_colname,", a.",object@cell_val_colname," FROM  ",object@matrix_table," a WHERE a.",object@matrix_id_colname," = ",object@matrix_id_value,") SELECT a.* FROM TABLE (FLMatrixDetUdt(z.Matrix_ID, z.Row_ID, z.Col_ID, z.Cell_Val) HASH BY z.Matrix_ID LOCAL ORDER BY z.Matrix_ID, z.Row_ID, z.Col_ID) AS a ORDER BY 1;")
-	retobj <- sqlQuery(connection,sqlstr)
-	result <- retobj$OutputDetVal
+	connection<-object@odbc_connection
+	sqlstr<-paste0("WITH z (Matrix_ID, Row_ID, Col_ID, Cell_Val) AS (SELECT a.",object@matrix_id_colname,", a.",object@row_id_colname,", a.",object@col_id_colname,", a.",object@cell_val_colname," FROM  ",object@matrix_table," a WHERE a.",object@matrix_id_colname," = ",object@matrix_id_value,") SELECT a.* FROM TABLE (FLMatrixDetUdt(z.Matrix_ID, z.Row_ID, z.Col_ID, z.Cell_Val) HASH BY z.Matrix_ID LOCAL ORDER BY z.Matrix_ID, z.Row_ID, z.Col_ID) AS a ORDER BY 1;")
+	retobj<-sqlQuery(connection,sqlstr)
+	result<-retobj$OutputDetVal
 	result
-} 
+}
 
+#overloading solve 
+solve <- function (x, ...){
+	UseMethod("solve", x)
+}
 
-	
+solve.FLMatrix<-function(object){
+	connection<-object@odbc_connection
+	sqlstr<-paste0("WITH z (Matrix_ID, Row_ID, Col_ID, Cell_Val) AS (SELECT a.",object@matrix_id_colname,", a.",object@row_id_colname,", a.",object@col_id_colname,", a.",object@cell_val_colname," FROM  ",object@matrix_table," a WHERE a.",object@matrix_id_colname," = ",object@matrix_id_value,") SELECT a.* FROM TABLE (FLMatrixInvUdt(z.Matrix_ID, z.Row_ID, z.Col_ID, z.Cell_Val) HASH BY z.Matrix_ID LOCAL ORDER BY z.Matrix_ID, z.Row_ID, z.Col_ID) AS a ORDER BY 1,2,3;")
+	retobj<-sqlQuery(connection,sqlstr)
+	data<-c()
+	i<-1
+	while(i<=length(retobj$OutputVal))
+	{
+		data<-append(data,retobj$OutputVal[i])
+		i<-i+1
+	}
+	options(scipen=999)
+	return <- round(data, digits=8)
+	inverse<-matrix(return, nrow=sqrt(length(retobj$OutputVal)), ncol=sqrt(length(retobj$OutputVal)), byrow=TRUE)
+	inverse
+}
