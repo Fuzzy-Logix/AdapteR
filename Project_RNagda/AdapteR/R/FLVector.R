925927bac02d5f5ada227878246e2ec0ede6d63d
psrikar <psrikar@35fd5345-5407-9947-aa4f-5170b9b8abad>
2015-09-10 20:36:01 +0000

    git-svn-id: https://192.168.2.203:8080/svn/RWrappers/trunk@175 35fd5345-5407-9947-aa4f-5170b9b8abad

--- Project_RNagda/AdapteR/R/FLVector.R
+++ Project_RNagda/AdapteR/R/FLVector.R
@@ -12,3 +12,15 @@ setClass(
 		col_name = "character"
 	)
 )
+
+is.FLVector <- function(object)
+{
+	ifelse(class(object)=="FLVector",TRUE,FALSE)
+	
+}
+
+FLVector <- function(table, col_name)
+{
+	sqlQuery(table@odbc_connection, paste0(" DATABASE ",table@db_name,"; SET ROLE ALL;"))
+	new("FLVector", table = table, col_name = col_name)
+}
