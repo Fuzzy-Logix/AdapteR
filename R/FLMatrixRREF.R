9d0fb7b32742f23bd7d1cea87f41636e60b52248
psrikar <psrikar@35fd5345-5407-9947-aa4f-5170b9b8abad>
2015-12-04 06:05:56 +0000

    restructured
    git-svn-id: https://192.168.2.203:8080/svn/RWrappers/trunk/AdapteR@198 35fd5345-5407-9947-aa4f-5170b9b8abad

--- R/FLMatrixRREF.R
+++ R/FLMatrixRREF.R
@@ -30,45 +30,29 @@ FLMatrixRREF <- function (x, ...){
 FLMatrixRREF.FLMatrix<-function(object)
 {
 
-	if(nrow(object) != ncol(object)) 
-	{ 
-		stop("FLMatrixRREF function is applicable on square matrix only") 
-	}
-
+	# checkSquare(object,"FLMatrixRREF")
 	connection<-object@odbc_connection
 	flag1Check(connection)
 
-	sqlstr<-paste0(" INSERT INTO ",result_db_name,".",result_matrix_table,
-				   " WITH z (Matrix_ID, Row_ID, Col_ID, Cell_Val) 
-						AS (SELECT a.",object@matrix_id_colname,", 
-								   a.",object@row_id_colname,", 
-								   a.",object@col_id_colname,", 
-								   a.",object@cell_val_colname,
-							" FROM  ",remoteTable(object)," a 
-							WHERE a.",object@matrix_id_colname," = ",object@matrix_id_value,") 
-					SELECT ",max_matrix_id_value,
-					       ",a.OutputRowNum,
-					        a.OutputColNum,
-					        a.OutputVal 
-					FROM TABLE (FLMatrixRREFUdt(z.Matrix_ID, z.Row_ID, z.Col_ID, z.Cell_Val) 
-						HASH BY z.Matrix_ID 
-						LOCAL ORDER BY z.Matrix_ID, z.Row_ID, z.Col_ID) AS a;")
+	sqlstr<-paste0(" INSERT INTO ",
+                   getRemoteTableName(result_db_name,result_matrix_table),
+                   viewSelectMatrix(object,"a",viewName="z"),
+                   outputSelectMatrix("FLMatrixRREFUdt",viewName="z",localName="a")
+                   )
 	
 	sqlSendUpdate(connection,sqlstr)
 	
+	MID <- max_matrix_id_value
 	max_matrix_id_value <<- max_matrix_id_value + 1
 
 	return(FLMatrix( 
 		       connection = connection, 
 		       database = result_db_name, 
 		       matrix_table = result_matrix_table, 
-			   matrix_id_value = max_matrix_id_value-1,
+			   matrix_id_value = MID,
 			   matrix_id_colname = "MATRIX_ID", 
 			   row_id_colname = "ROW_ID", 
 			   col_id_colname = "COL_ID", 
 			   cell_val_colname = "CELL_VAL",
-			   nrow = nrow(object), 
-			   ncol = ncol(object), 
-			   dimnames = list(c(),c()))
-	      )
+			))
 }
