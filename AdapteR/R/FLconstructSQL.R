ceb1595ce827dabceedcb2564dc5b7d1ed172858
gkappler <gkappler@35fd5345-5407-9947-aa4f-5170b9b8abad>
2016-02-20 17:40:08 +0000

    refactored constructSelect methods
    git-svn-id: https://192.168.2.203:8080/svn/RWrappers/trunk@254 35fd5345-5407-9947-aa4f-5170b9b8abad

---
+++ AdapteR/R/FLconstructSQL.R
@@ -0,0 +1,270 @@
+
+
+##' 
+##' .. content for \details{} ..
+##' @title 
+##' @param object 
+##' @param ... 
+##' @return 
+##' @author  Gregor Kappler <g.kappler@@gmx.net>
+setGeneric("constructSelect", function(object,...) {
+    standardGeneric("constructSelect")
+})
+
+setMethod("constructSelect",
+          signature(object = "FLMatrix"),
+          function(object,joinNames=TRUE){
+              ##browser()
+              if(!FLNamesMappedP(object) | !joinNames)
+                  return(constructSelect(object@select))
+              select <- object@select
+              select@variables <- c(select@variables,
+                                    object@mapSelect@variables)
+              select@table_name <- c(select@table_name,
+                                     object@mapSelect@table_name)
+              select@whereconditions <- c(select@whereconditions,
+                                          object@mapSelect@whereconditions)
+              return(constructSelect(select))
+          })
+
+setMethod("constructSelect",
+          signature(object = "FLTableQuery"),
+          function(object) {
+              if(is.null(object@database)) return(NULL)
+              if(length(object@database)==0) return(NULL)
+              return(paste0("SELECT ",
+                            paste(colnames(object),collapse=", "),
+                            " FROM ",remoteTable(object),
+                            constructWhere(c(constraintsSQL(object))),
+                            paste(object@order,collapse=", "),
+                            "\n"))
+          })
+
+
+setMethod("constructSelect",
+          signature(object = "FLTable"),
+          function(object) {
+            if(class(object@select)=="FLTableFunctionQuery") 
+            return(constructSelect(object@select))
+              if(!object@isDeep) 
+              {
+                variables <- getVariables(object)
+                if(is.null(names(variables)))
+                    names(variables) <- variables
+                else
+                    names(variables)[is.na(names(variables))] <- variables[is.na(names(variables))]
+
+                ifelse(is.null(variables$obs_id_colname),vobsIDCol <- variables["vectorIndexColumn"],
+                   vobsIDCol <- variables["obs_id_colname"])
+                colnames <- colnames(object)
+                #colnames <- colnames[colnames!=vobsIDCol]
+                newColnames <- renameDuplicates(colnames)
+                variables <- as.list(c(vobsIDCol[[1]],colnames))
+                names(variables) <- c("obs_id_colname",
+                                      newColnames)
+
+                return(paste0(
+                            "SELECT\n",
+                            paste0("     ",
+                                   variables," AS ",
+                                   names(variables),
+                                   collapse = ",\n"),
+                            "\nFROM ",remoteTable(object),
+                            constructWhere(c(constraintsSQL(object))),
+                            "\n"))
+              }
+              else 
+              {
+                variables <- getVariables(object)
+                if(is.null(names(variables)))
+                  names(variables) <- variables
+                else
+                    names(variables)[is.na(names(variables))] <- variables[is.na(names(variables))]
+
+                variables <- as.list(c(variables[["obs_id_colname"]],variables[["var_id_colname"]],variables[["cell_val_colname"]]))
+                names(variables) <- c("obs_id_colname","var_id_colname","cell_val_colname")
+                return(paste0(
+                          "SELECT\n",
+                          paste0("     ",
+                                 variables," AS ",
+                                 names(variables),
+                                 collapse = ",\n"),
+                          "\nFROM ",remoteTable(object),
+                          constructWhere(c(constraintsSQL(object))),
+                          "\n"))
+              }
+          })
+
+setMethod("constructSelect", signature(object = "FLVector"),
+          function(object) {
+            if(class(object@select)=="FLTableFunctionQuery") 
+            return(constructSelect(object@select))
+              if(!object@isDeep) {
+                newColnames <- renameDuplicates(colnames(object))
+                variables <- getVariables(object)
+                if(is.null(names(variables)))
+                    names(variables) <- variables
+                else
+                    names(variables)[is.na(names(variables))] <- variables[is.na(names(variables))]
+
+                ifelse(is.null(variables$obs_id_colname),vobsIDCol <- variables["vectorIndexColumn"],
+                   vobsIDCol <- variables["obs_id_colname"])
+                variables <- as.list(c("'%insertIDhere%'",vobsIDCol[[1]],colnames(object)))
+                names(variables) <- c("vectorIdColumn",
+                                      "vectorIndexColumn",
+                                      if(length(colnames(object))>1) newColnames else "vectorValueColumn")
+
+                return(paste0(
+                            "SELECT\n",
+                            paste0("     ",
+                                   variables," AS ",
+                                   names(variables),
+                                   collapse = ",\n"),
+                            "\nFROM ",remoteTable(object),
+                            constructWhere(c(constraintsSQL(object))),
+                            "\n"))
+              } else {
+                  variables <- getVariables(object)
+                  if(is.null(names(variables)))
+                    names(variables) <- variables
+                  else
+                      names(variables)[is.na(names(variables))] <- variables[is.na(names(variables))]
+
+                  if(nrow(object)==1)
+                  vobsIDCol <- variables["var_id_colname"]
+                  else vobsIDCol <- variables["obs_id_colname"]
+                  variables <- as.list(c("'%insertIDhere%'",vobsIDCol[[1]],variables[["cell_val_colname"]]))
+                  names(variables) <- c("vectorIdColumn","vectorIndexColumn","vectorValueColumn")
+                  return(paste0(
+                            "SELECT\n",
+                            paste0("     ",
+                                   variables," AS ",
+                                   names(variables),
+                                   collapse = ",\n"),
+                            "\nFROM ",remoteTable(object),
+                            constructWhere(c(constraintsSQL(object))),
+                            "\n"))
+              }
+          })
+
+constructVariables <- function(variables){
+    if(!is.null(names(variables)))
+        return(paste0("     ",
+                      variables, " ",
+                      names(variables),
+                      collapse = ",\n"))
+    else
+        return(paste0("     ",
+                      variables, " ",
+                      collapse = ",\n"))
+               
+}
+setMethod(
+    "constructSelect",
+    signature(object = "FLSelectFrom"),
+    function(object) {
+        if(is.null(object@database)) return(NULL)
+        if(length(object@database)==0) return(NULL)
+        variables <- getVariables(object)
+        order <- setdiff(object@order,c(NA,""))
+        if(length(order)==0)
+            ordering <- ""
+        else
+            ## ordering <- paste0(" ORDER BY ",paste0(object@obs_id_colname,collapse = ", "))
+            ordering <- paste0("\nORDER BY ",
+                               paste0(order,
+                                      collapse = ", "))
+        return(paste0(
+            "SELECT\n",
+            constructVariables(variables),
+            "\nFROM ",remoteTable(object),
+            constructWhere(c(constraintsSQL(object))),
+            ordering,
+            "\n"))
+    })
+
+setMethod("constructSelect",
+          signature(object = "FLTableFunctionQuery"),
+          function(object) return(object@SQLquery))
+
+setMethod("constructSelect",
+          signature(object = "numeric"),
+          function(object) return(object))
+
+setMethod("constructSelect",
+          signature(object = "character"),
+          function(object) return(object))
+
+
+
+## Phani-- removed \n as it was creating problem in FLCorrel test cases
+constructWhere <- function(conditions) {
+    if(!is.character(conditions))
+        stop("Provide constraints as character vector")
+    conditions <- setdiff(conditions,c(NA,""))
+    if(length(conditions)>0)
+        paste0(" WHERE",paste0("   (",conditions,")",
+                                collapse=" AND "))
+    else
+        ""
+}
+
+setGeneric("viewSelectMatrix", function(object,localName, withName) {
+    standardGeneric("viewSelectMatrix")
+})
+setMethod("viewSelectMatrix", signature(object = "FLMatrix",
+                                        localName="character",
+                                        withName="missing"),
+          function(object,localName, withName="z")
+          {viewSelectMatrix(object,localName,withName="z")})
+setMethod("viewSelectMatrix", signature(object = "FLMatrix",
+                                        localName="character",
+                                        withName="character"),
+          function(object,localName, withName="z") {
+              ##' todo: alias
+              ##names(object@select@table_name)[[1]] <- localName
+              object <- orderVariables(
+                  updateVariable(object,"Matrix_ID",-1),
+                  c("Matrix_ID","rowIdColumn","colIdColumn","valueColumn")
+              )
+              return(paste0(" WITH ",withName,
+                            " (Matrix_ID, Row_ID, Col_ID, Cell_Val)
+              AS (",constructSelect(object,joinNames=FALSE)," ) "))
+          })
+
+## outputSelectMatrix apples function given by func_name to view given by viewname
+## and returns columns specified by outcolnames list. IncludeMID tells if max_matrix_id_value
+## should be one of the columns returned.
+setGeneric("outputSelectMatrix",
+           function(func_name,
+                    viewName,
+                    localName,...) {
+    standardGeneric("outputSelectMatrix")
+})
+
+setMethod("outputSelectMatrix", signature(func_name="character",
+                                          viewName="character",
+                                          localName="character"),
+          function(func_name,
+                   viewName,
+                   localName,
+                   includeMID=FALSE,
+                   outColNames=list(rowIdColumn="OutputRowNum",
+                                    colIdColumn="OutputColNum",
+                                    valueColumn="OutputVal"),
+                   whereClause=";",...)
+          {
+              return(paste0(" SELECT ",
+                            ifelse(!includeMID,"",
+                                   "'%insertIDhere%' AS OutputMatrixID, "),
+                            constructVariables(outColNames),
+                            "\nFROM TABLE (",func_name,
+                            "(",viewName,".Matrix_ID, ",
+                            viewName,".Row_ID, ",
+                            viewName,".Col_ID, ",
+                            viewName,".Cell_Val)",
+                            " HASH BY z.Matrix_ID 
+          LOCAL ORDER BY z.Matrix_ID, z.Row_ID, z.Col_ID ",
+          ") AS ",localName," ",whereClause))
+          })
+
