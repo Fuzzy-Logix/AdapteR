55654c439d6809b7b09570478f79d6853e7fbbb2
AnanyaT <tripathi.anany@gmail.com>
2016-06-02 18:49:25 +0530

    Initial commit

--- R/FLFKMeans.R
+++ R/FLFKMeans.R
@@ -570,6 +570,7 @@ silinfo.FLFKMeans <- function(object){
 		c <- paste0(getOption("ResultDatabaseFL"),".",gen_unique_table_name("3"))
 		d <- paste0(getOption("ResultDatabaseFL"),".",gen_unique_table_name("4"))
 		e <- paste0(getOption("ResultDatabaseFL"),".",gen_unique_table_name("5"))
+		browser()
 
 		##Ensure required temptables exist
 		if(is.null(object@temptables[["temptbl4"]]))
@@ -647,6 +648,7 @@ silinfo.FLFKMeans <- function(object){
 			ObsID <- rownames(widthsDataFrame)
 			widthsDataFrame$obs_id_colname <- NULL
 			widthsmatrix <- as.matrix(widthsDataFrame)
+			widthsmatrix <- matrix(as.numeric(widthsmatrix),nrow(widthsmatrix))
 			rownames(widthsmatrix) <- ObsID
 			colnames(widthsmatrix) <- c("cluster","neighbor","sil_width")
 		}
