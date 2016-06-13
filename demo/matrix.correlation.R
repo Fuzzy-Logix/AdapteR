a69133cfc39078d61866b95916ece558a849189b
Gregor Kappler <g.kappler@gmx.net>
2016-06-13 15:31:13 +0200

    use connection demo in other demos

--- demo/matrix.correlation.R
+++ demo/matrix.correlation.R
@@ -12,39 +12,9 @@
 ## The demo highlights how to build an
 ## interactive stock returns correlation demo
 ## computed in database!
-
-## require(AdapteR)
-
-## Setting up a connection can be either done with
-## ODBC or JDBC
-## Setting up a ODBC connection
-if(!exists("yourODBCSource") & !exists("yourUser"))
-    stop("Please set the variable \nyourODBCSource <- \"...\" for odbc login!\nor set for jdbc login:\nyourUser <- \"...\"\nyourPassword <- \"...\"")
-
-if(!exists("connection") & exists("yourODBCSource")){
-    connection <- flConnect(odbcSource = yourODBCSource)
+if(!exists("connection")) {
+    demo("connecting", package="AdapteR")
 }
-
-## If ODBC has failed we try to create a JDBC connection
-if(!exists("connection")){
-    if(!exists("yourUser")) stop("Please set the variable \nyourUser <- \"...\" for jdbc login!")
-    if(!exists("yourPassword")) stop("Please set the variable \nyourPassword <- \"...\" for jdbc login!")
-    if(!exists("yourJarDir")) yourJarDir <- NULL
-    connection <-
-        flConnect(
-            host     = "10.200.4.116",
-            database = "Fl_demo",
-            user = yourUser,
-            passwd = yourPassword,
-            ## set dir.jdbcjars to add jdbc driver
-            ## and security jars to classpath:
-            ##    terajdbc4.jar tdgssconfig.jar
-            ## CAVE: fully qualified PATH required
-            dir.jdbcjars = yourJarDir)
-    if(!exists("connection")) stop("Please check your username and password\nand possibly set the variable \nyourPassword <- \"...\" for jdbc login!")
-}
-
-readline("press any key to continue")
 
 #############################################################
 ## For in-database analytics the matrix is in the warehouse
@@ -52,7 +22,7 @@ readline("press any key to continue")
 sqlQuery(connection,
            "select top 10 * from FL_DEMO.finEquityReturns")
 
-readline("press any key to continue")
+readline("The table consists of triples that define a matrix of equity returns")
 
 ###########################################################
 ## Correlation Matrix
@@ -67,15 +37,14 @@ SELECT  a.TickerSymbol           AS Ticker1,
 FROM    FL_DEMO.finEquityReturns a,
         FL_DEMO.finEquityReturns b
 WHERE   b.TxnDate = a.TxnDate
-AND     a.TickerSymbol IN ('AAPL','HPQ','IBM',
-                           'MSFT','ORCL')
+AND     a.TickerSymbol IN ('AAPL')
 AND     b.TickerSymbol IN ('AAPL','HPQ','IBM',
                            'MSFT','ORCL')
 GROUP BY a.TickerSymbol,
          b.TickerSymbol
 ORDER BY 1, 2;")
 
-readline("press any key to continue")
+readline("The SQL-through R way to compute a correlation matrix with DB Lytix.")
 
 ## A remote matrix is easily created by specifying
 ## table, row id, column id and value columns
@@ -111,15 +80,12 @@ options(debugSQL=TRUE)
 ## Note that no SQL is sent when defining data-sets
 ## 1. select the desired colums from the full matrix
 sm <- eqnRtn[,c('AAPL','HPQ','IBM','MSFT','ORCL')]
-
-readline("press any key to continue")
-
-## 2. select the desired colums from the full matrix
+## 2. use the default R 'cor' function
 flCorr <- cor(sm)
-## Note that SQL is sent when data is printed or otherwise used
-flCorr
+readline("Note that SQL is not sent yet during definition")
 
-readline("press any key to continue")
+flCorr
+readline("Note that SQL is sent when data is printed or otherwise used")
 
 ## Casting methods fetch (selected) data from the warehouse into R memory
 rEqnRtn <- as.matrix(eqnRtn[,c('AAPL','HPQ','IBM','MSFT','ORCL')])
@@ -128,8 +94,9 @@ rEqnRtn <- na.omit(rEqnRtn)
 ## the result is in the same format as the R results
 rCorr <- cor(rEqnRtn, rEqnRtn)
 round(rCorr,2)
-
 round(flCorr,2)
+
+readline("Note: The result is in the same format as the R results.")
 
 
 ########################################
@@ -148,12 +115,11 @@ eqnRtn[dec2006, c("HPQ","MSFT")]
 
 readline("press any key to continue")
 
-## NO SQL is sent during the definition of subsetting
 E <- eqnRtn[dec2006, randomstocks]
+readline("NO SQL is sent during the definition of subsetting")
 
-## Data is fetched on demand only, e.g. when printing
 print(E)
-readline("press any key to continue")
+readline("Data is fetched on demand only, e.g. when printing")
 
 
 
@@ -171,171 +137,77 @@ heatmap.2(as.matrix(M),
           col=redgreen(100),
           cexCol = 1,
           cexRow = 1)
+readline("You can use FL results in other R packages, e.g. plotting -- or shiny (next)")
 
 
+run.FLCorrelationShiny <- function (){
 ###########################################################
-## Shiny Correlation Plot Demo
-##
-## metadata can be easily combined on the client
-## download metadata from
-## https://raw.githubusercontent.com/aaronpk/Foursquare-NASDAQ/master/companylist.csv
-metaInfo <- read.csv(
-    "/Users/gregor/Downloads/companylist.csv")
+    ## Shiny Correlation Plot Demo
+    ##
+    ## metadata can be easily combined on the client
+    ## download metadata from
+    metaInfo <- read.csv("https://raw.githubusercontent.com/aaronpk/Foursquare-NASDAQ/master/companylist.csv")
 
-## metadata contains sectors and industries
-## that will be selectable in the shiny web ui
-table(metaInfo$industry)
-table(metaInfo$Sector)
+    ## metadata contains sectors and industries
+    ## that will be selectable in the shiny web ui
+    table(metaInfo$industry)
+    table(metaInfo$Sector)
+    stockCorrelPlot <- function(input){
+        ## get selected and available ticker symbols
+        metastocks <- as.character(
+            metaInfo$Symbol[
+                metaInfo$industry %in% input$industries |
+                metaInfo$Sector %in% input$sectors])
+        stocks <- intersect(
+            unique(c(input$stocks,
+                     metastocks)),
+            colnames(eqnRtn))
 
+        ## compute correlation matrix
+        flCorr <- as.matrix(cor(eqnRtn[,stocks]))
 
+        ## plot with company names and stocks
+        rownames(flCorr) <- metaInfo$Name[
+            match(rownames(flCorr),
+                  metaInfo$Symbol)]
+        heatmap.2(flCorr, symm=TRUE,
+                  distfun=function(c) as.dist(1 - c),
+                  trace="none",
+                  col=redgreen(100),
+                  cexCol = 1, srtCol=90,
+                  cexRow = 1)
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-stockCorrelPlot <- function(input){
-    ## get selected and available ticker symbols
-    metastocks <- as.character(
-        metaInfo$Symbol[
-            metaInfo$industry %in% input$industries |
-            metaInfo$Sector %in% input$sectors])
-    stocks <- intersect(
-        unique(c(input$stocks,
-                 metastocks)),
-        colnames(eqnRtn))
-
-    ## compute correlation matrix
-    flCorr <- as.matrix(cor(eqnRtn[,stocks]))
-
-    ## plot with company names and stocks
-    rownames(flCorr) <- metaInfo$Name[
-        match(rownames(flCorr),
-              metaInfo$Symbol)]
-    heatmap.2(flCorr, symm=TRUE,
-              distfun=function(c) as.dist(1 - c),
-              trace="none",
-              col=redgreen(100),
-              cexCol = 1, srtCol=90,
-              cexRow = 1)
+    require(R.utils)
+    require(shiny)
+    shinyApp(
+        ui = fluidPage(
+            fluidRow(
+                column(3,
+                       selectInput(
+                           "sectors", "Sectors:",
+                           choices = levels(metaInfo$Sector),
+                           selected = "Energy",
+                           multiple = TRUE)),
+                column(3,
+                       selectInput(
+                           "industries", "Industries:",
+                           choices = levels(metaInfo$industry),
+                           selected = "Commercial Banks",
+                           multiple = TRUE)),
+                column(6,
+                       selectInput(
+                           "stocks", "Stocks:",
+                           choices = colnames(eqnRtn),
+                           selected = c(),
+                           multiple = TRUE))),
+            fluidRow(plotOutput("correlations"))
+        ),
+        server = function(input, output) {
+        output$correlations <- renderPlot(
+            stockCorrelPlot(input), height=800)
+    }
+    )
 }
 
-require(R.utils)
-require(shiny)
-shinyApp(
-    ui = fluidPage(
-        fluidRow(
-            column(3,
-                   selectInput(
-                       "sectors", "Sectors:",
-                       choices = levels(metaInfo$Sector),
-                       selected = "Energy",
-                       multiple = TRUE)),
-            column(3,
-                   selectInput(
-                       "industries", "Industries:",
-                       choices = levels(metaInfo$industry),
-                       selected = "Commercial Banks",
-                       multiple = TRUE)),
-            column(6,
-                   selectInput(
-                       "stocks", "Stocks:",
-                       choices = colnames(eqnRtn),
-                       selected = c(),
-                       multiple = TRUE))),
-        fluidRow(plotOutput("correlations"))
-    ),
-    server = function(input, output) {
-        output$correlations <- renderPlot(
-          stockCorrelPlot(input), height=800)
-    }
-)
-
-
-###########################################################
-##
-## Matrix Inversion
-##
-## The SQL-through R way a la Manual:
-sqlQuery(connection, "
-WITH z (Matrix_ID, Row_ID, Col_ID, NumVal) AS
-(
-SELECT a.Matrix_ID,
-       a.Row_ID,
-       a.Col_ID,
-       a.Cell_Val
-FROM fl_dev.tblMatrixMulti a
-WHERE a.Matrix_ID = 5
-)
-SELECT a.*
-FROM TABLE (
-     FLMatrixInvUdt(z.Matrix_ID, z.Row_ID, z.Col_ID, z.NumVal) HASH BY z.Matrix_ID
-  LOCAL ORDER BY z.Matrix_ID, z.Row_ID, z.Col_ID
-) AS a
-ORDER BY 1,2,3;")
-
-
-
-
-
-
-
-m <- FLMatrix(database          = "FL_DEMO",
-              table_name        = "tblMatrixMulti",
-              matrix_id_colname = "Matrix_ID",
-              matrix_id_value   = "5",
-              row_id_colname    = "Row_ID",
-              col_id_colname    = "Col_ID",
-              cell_val_colname  = "Cell_Val")
-
-## compute inverse in R after
-## fetching data by a simple as.matrix cast call
-rm <- as.matrix(m)
-solve(rm)
-
-## compute the inverse in-database
-ms <- solve(m)
-ms
-
-## in-databse matrix multiplication with inverse
-## results in identity matrix (of course)
-round(as.matrix(m %*% ms))
-
-
-
-
-
-
-
-
-
-
-
-
-## check is R and DB Lytix results match up:
-m.r <- as.matrix(m) ## download and convert to R matrix
-expect_equal(as.matrix(ms),
-             solve(m.r),check.attributes=FALSE)
-
-
-options(debugSQL=TRUE)
-flM <- as.FLMatrix(matrix(runif(25),5))
-
-flM
-
-flM + flM
-
-flM - flM
-
-flM / flM
-
-a <- (flM %*% flM) - flM
-a
-
-solve(flM) %*% flM - flM
+readline("To explore correlations interactively, we defined a function above. \n Simply execute\n> run.FLCorrelationShiny()\nafter ending the Demo.\nEnd the demo now:")
