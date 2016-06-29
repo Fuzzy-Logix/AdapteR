305148709fe0e19e38f886ce2f11162800ec39b4
Gregor Kappler <g.kappler@gmx.net>
2016-06-29 18:54:56 +0200

    fix chol tests

--- tests/testthat/test_chol.R
+++ tests/testthat/test_chol.R
@@ -1,21 +1,20 @@
 
-x <- matrix(c(1:5, (1:5)^2), 5, 2)
-x <- cbind(x, x[, 1] + 3*x[, 2])
-colnames(x) <- letters[20:22]
-m <- Matrix::crossprod(x)
 
 Renv = new.env(parent = globalenv())
+Renv$x <- matrix(c(1:5, (1:5)^2), 5, 2)
+Renv$x <- cbind(Renv$x, Renv$x[, 1] + 3*Renv$x[, 2])
+colnames(Renv$x) <- letters[20:22]
 Renv$mat1 = matrix(c(5,1,1,3),2,2)
-Renv$mat2 = m
 Renv$mat3 = matrix(c(5,-5,-5,3), 2, 2)
 
 FLenv <- as.FL(Renv)
 
 test_that("chol of positive definite matrix ",{
     result = eval_expect_equal({
+        m <- crossprod(x)
         test1 = chol(mat1)},
         Renv,FLenv)
-    print(result)
+    ##print(result)
     })
  
 #FL Works, R works only if pivot=TRUE, in which
@@ -24,7 +23,7 @@ test_that("chol of positive semi-definite ",{
     result = eval_expect_equal({
         test2 = chol(mat2)},
         Renv,FLenv)
-    print(result)
+    ##print(result)
     })
 
 #FL fails as expected. R works only if pivot=TRUE
@@ -32,6 +31,6 @@ test_that("chol of non-positive-definite https://app.asana.com/0/143316600934101
     result = eval_expect_equal({
         test3 = chol(mat3,pivot = TRUE)},
         Renv,FLenv)
-    print(result)
-    })
+    ##print(result)
+})
 
