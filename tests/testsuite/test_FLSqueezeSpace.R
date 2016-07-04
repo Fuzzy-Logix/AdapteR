3d8f02bb814969884aee068bdf7aae8e87cac8fa
Gregor Kappler <g.kappler@gmx.net>
2016-07-04 13:00:47 +0200

    tests of FLSqueezeSpace

--- tests/testsuite/test_FLSqueezeSpace.R
+++ tests/testsuite/test_FLSqueezeSpace.R
@@ -1,22 +1,14 @@
+                                        #Not in R .
+test_that("FLSqueezeSpace",{
+    widetable  <- FLTable("FL_DEMO", "tblstringID", "stringID")
+    flv <- widetable[1:6,"string"]
+    resultflvector <- FLSqueezeSpace(flv)
+    expect_equal(as.R(flv),as.R(resultflvector))
 
-
-#Not in R .
-#For ODBC Connection with FL_DEMO database.
-
-test_that("Check for FLCleanStr function",{
-         widetable  <- FLTable("FL_DEMO", "tblstringID", "stringID")
-         flv <- widetable[1:6,"string"]
-         print(flv)
-         resultflvector <- FLSqueezeSpace(flv)
-         print(resultflvector)
-       })
-
-#Not in R .
-#For JDBC Connection with fuzzylogix database.
-test_that("Check for FLCleanStr function",{
-         widetable  <- FLTable("fuzzylogix", "tblAutoMpg", "ObsID")
-         flv <- widetable[1:6,"CarName"]
-         print(flv)
-         resultflvector <- FLSqueezeSpace(flv)
-         print(resultflvector)
-       })
+    singleSpace <- c("DU ANE", "Home Depot", "MARH TA", "WAL MA RT", "WALMART", "WARTHA")
+    multiSpace <- gsub(" ","   ",singleSpace)
+    flv <- as.FL(multiSpace)
+    expect_equal(singleSpace,as.R(resultflvector))
+    resultflvector <- FLSqueezeSpace(flv)
+    expect_equal(singleSpace,as.R(resultflvector))
+})
