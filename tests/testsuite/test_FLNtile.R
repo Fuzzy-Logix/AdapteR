5cf189053fb282d5bfbf66959559ca8def07154b
Gregor Kappler <g.kappler@gmx.net>
2016-07-04 13:12:52 +0200

    tests for FLNtile, Widetodeep, det and rowmeans, colmeans

--- tests/testsuite/test_FLNtile.R
+++ tests/testsuite/test_FLNtile.R
@@ -1,21 +1,25 @@
-
+N <- 100
+M <- 60
 #Data objects initialisation.
 Renv = new.env(parent = globalenv())
-Renv$vector1 = sample(1:1000,600,replace = TRUE)
-Renv$matrix1 = matrix(sample(1:1000,600,replace = TRUE), nrow = 200, ncol = 300, byrow = TRUE)
-Renv$dataframe1 = data.frame(sample(1:1000,600,replace = TRUE), sample(1:1000,600,replace = TRUE))
+Renv$vector1 = sample(1:N,M,replace = TRUE)
+Renv$matrix1 = matrix(sample(1:N,M,replace = TRUE), nrow = 20, byrow = TRUE)
+Renv$dataframe1 = data.frame(a=sample(1:N,M,replace = TRUE),
+                             b=sample(1:N,M,replace = TRUE))
 FLenv = as.FL(Renv)
 
 #Divide data object into n buckets.
 #SQL Query taking too much time.
 #Only Deep table is accepted by getDescStatsUDT.
-
-test_that("Check for FLNtile function of AdapteR",{
+##options(debugSQL=T)
+test_that("FLNtile: returning correct number of values: https://app.asana.com/0/150173007236461/146668378502512",{
     n = 10
     result1 = FLNtile(FLenv$vector1,n)
-    ##    print(result1)
-    result2 = FLNtile(FLenv$matrix1,n)
-    ##    print(result2)
-    result3 = FLNtile(FLenv$dataframe1,n)
-    ##    print(result3)
-    })
+    expect_equal(length(result1),length(FLenv$vector1))
+    expect_equal(length(as.vector(result1)),length(FLenv$vector1))
+    ##print(result1)
+    ##result2 = FLNtile(FLenv$matrix1,n)
+    ##print(result2)
+    ##result3 = FLNtile(FLenv$dataframe1,n)
+    ##print(result3)
+})
--- tests/testsuite/test_rowcolsummeans.R
+++ tests/testsuite/test_rowcolsummeans.R
@@ -7,7 +7,7 @@ Renv$M <- bdiag(Diagonal(2), matrix(1:3, 3,4), diag(3:2))
 Renv$M <- as.array(Renv$M)
 
 
-
+FLenv <- as.FL(Renv)
 #colSums(M)
 #rowSums(M)
 #colMeans(M)
@@ -18,23 +18,19 @@ Renv$M <- as.array(Renv$M)
 #Tests
 
 test_that("Check1 for colSums function",{
-  result = eval_expect_equal({colSums(M) },Renv)
-  ##  print(result)
+  result = eval_expect_equal({colSums(M) },Renv, FLenv)
 })
 
 test_that("Check2 for rowSums function",{
-  result = eval_expect_equal({rowSums(M) },Renv,)
-  ##  print(result)
+  result = eval_expect_equal({rowSums(M) },Renv,FLenv)
 })
 
 test_that("Check3 for colMeans function",{
-  result = eval_expect_equal({colMeans(M) },Renv)
-  ##  print(result)
+  result = eval_expect_equal({colMeans(M) },Renv,FLenv)
 })
 
 test_that("Check4 for rowMeans function",{
-  result = eval_expect_equal({rowMeans(M) },Renv)
-  ##  print(result)
+  result = eval_expect_equal({rowMeans(M) },Renv,FLenv)
 })
 
 #Creating a digaonal numeric matrix MM: a result of kronecker product
