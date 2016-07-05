49933968a9c70f2aaecf9f3381979a2d0eb30d63
Gregor Kappler <g.kappler@gmx.net>
2016-07-05 17:30:47 +0200

    fixed rankMatrix tests

--- tests/alltests.R
+++ tests/alltests.R
@@ -1,9 +1,13 @@
 library(testthat)
 library(AdapteR)
 
-runMyTestFile <- function(f){
+runMyTestFile <- function(f, ask=TRUE){
     cat(paste0("testing ",f,"\n"))
-    if(readline("start (y or n)")=="y")
+    if(ask)
+        run <- readline("start (y or n)")=="y"
+    else
+        run <- TRUE
+    if(run)
         tryCatch({
             options(debugSQL=FALSE)
             test_file(f)
@@ -11,8 +15,7 @@ runMyTestFile <- function(f){
         error=function(e) print(e))
 }
 
-llply(find_test_scripts("AdapteR/tests/testsuite"), runMyTestFile)
+llply(find_test_scripts("AdapteR/tests/testsuite"), runMyTestFile, ask=FALSE)
 
 
 llply(find_test_scripts("AdapteR/tests/limitations"), runMyTestFile)
-
