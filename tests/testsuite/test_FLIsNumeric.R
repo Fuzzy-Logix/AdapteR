830dfbd80f0b9bb68bc9a6d78f378e982bd3f1ac
Dhruv Pachauri <dhruvsai1151@gmail.com>
2016-07-05 19:01:26 +0530

    Moved failing test files to limitations.Fixed Minor Bugs..

--- tests/testsuite/test_FLIsNumeric.R
+++ tests/testsuite/test_FLIsNumeric.R
@@ -13,7 +13,7 @@
 
 #Not in R .
 #For JDBC Connection with fuzzylogix database.
-test_that("Check for FLCleanStr function",{
+test_that("Check for FLIsNumeric function",{
          widetable  <- FLTable(getOption("ResultDatabaseFL"), "tblAutoMpg", "ObsID")
          flv <- widetable[1:6,"CarName"]
          ##         print(flv)
