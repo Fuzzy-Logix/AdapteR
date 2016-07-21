5c9b939546b77e9d23ea9ef5e8378a142d723136
Amal <amal.shrivastava@fuzzylogix.com>
2016-07-21 18:21:15 +0530

    step function for FL throws errors

--- tests/testthat/test_step.R
+++ tests/testthat/test_step.R
@@ -1,26 +1,26 @@
-connection <- flConnect(odbcSource = "Gandalf",database = "FL_TRAIN",platform="TD")
+	connection <- flConnect(odbcSource = "Gandalf",database = "FL_TRAIN",platform="TD")
 
-## Since both the objects need to be dealt differently, objects have been initialized and 
-## defined outside test_that function
+	## Since both the objects need to be dealt differently, objects have been initialized and 
+	## defined outside test_that function
 
-FLenv$stepobj <- FLTable("FL_DEMO", "tblAbaloneWide", "ObsID")
-Renv$widetable<-as.data.frame(FLenv$stepobj)
+	FLenv$stepobj <- FLTable("FL_DEMO", "tblAbaloneWide", "ObsID")
+	Renv$widetable<-as.data.frame(FLenv$stepobj)
 
-## Since FL and R environments have different case structure for column names
-colnames(Renv$widetable)<-colnames(FLenv$stepobj)
+	## Since FL and R environments have different case structure for column names
+	colnames(Renv$widetable)<-colnames(FLenv$stepobj)
 
-Renv$stepobj<-lm(Diameter~Height+ShellWeight, data = Renv$widetable)
+	Renv$stepobj<-lm(Diameter~Height+ShellWeight, data = Renv$widetable)
 
-## step function does not work properly. throws an error
-## asana ticket- https://app.asana.com/0/143316600934101/158092657328842
+	## step function does not work properly. throws an error
+	## asana ticket- https://app.asana.com/0/143316600934101/158092657328842
 
-test_that("test for step", {
-  eval_expect_equal({
-    
-   r<-step(stepobj,scope = list(lower=Diameter~Height,upper=Diameter~Height+ShellWeight+ShuckedWeight), direction = "backward")
-    
-  },Renv,FLenv,
-  )
-  
-}
-)
+	test_that("test for step", {
+	  eval_expect_equal({
+	    
+	   r<-step(stepobj,scope = list(lower=Diameter~Height,upper=Diameter~Height+ShellWeight+ShuckedWeight), direction = "backward")
+	    
+	  },Renv,FLenv,
+	  )
+	  
+	}
+	)
