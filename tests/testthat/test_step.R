97cb4a4547a027d53c1daf23991b245f72323644
Amal <amal.shrivastava@fuzzylogix.com>
2016-07-25 10:53:35 +0530

    minor changes to test_step

--- tests/testthat/test_step.R
+++ tests/testthat/test_step.R
@@ -1,26 +1,56 @@
-	connection <- flConnect(odbcSource = "Gandalf",database = "FL_TRAIN",platform="TD")
+connection <- flConnect(odbcSource = "Gandalf",database = "FL_DEMO",platform="TD")
+devtools::load_all(".")
+Renv = new.env(parent = globalenv())
 
-	## Since both the objects need to be dealt differently, objects have been initialized and 
-	## defined outside test_that function
+FLenv = as.FL(Renv)
 
-	FLenv$stepobj <- FLTable("FL_DEMO", "tblAbaloneWide", "ObsID")
-	Renv$widetable<-as.data.frame(FLenv$stepobj)
+## Since both the objects need to be dealt differently, objects have been initialized and 
+## defined outside test_that function
 
-	## Since FL and R environments have different case structure for column names
-	colnames(Renv$widetable)<-colnames(FLenv$stepobj)
+FLenv$widetable <- FLTable("FL_DEMO", "tblAbaloneWide", "ObsID")
+Renv$widetable<-as.data.frame(FLenv$widetable)
 
-	Renv$stepobj<-lm(Diameter~Height+ShellWeight, data = Renv$widetable)
+## Since FL and R environments have different case structure for column names
+colnames(Renv$widetable)<-colnames(FLenv$widetable)
 
-	## step function does not work properly. throws an error
-	## asana ticket- https://app.asana.com/0/143316600934101/158092657328842
+Renv$stepobj<-lm(Diameter~Height+ShellWeight, data = Renv$widetable)
 
-	test_that("test for step", {
-	  eval_expect_equal({
-	    
-	   r<-step(stepobj,scope = list(lower=Diameter~Height,upper=Diameter~Height+ShellWeight+ShuckedWeight), direction = "backward")
-	    
-	  },Renv,FLenv,
-	  )
-	  
-	}
-	)
+## step function does not work properly. throws an error
+## asana ticket- https://app.asana.com/0/143316600934101/158092657328842
+
+options(debugSQL = T)
+
+stepobj <- Renv$stepobj
+stepobj <- FLenv$stepobj
+
+
+test_that("test for step", {
+  eval_expect_equal({
+    
+    r<-step(stepobj,scope = list(lower=Diameter~Height,upper=Diameter~Height+ShellWeight+ShuckedWeight), direction = "backward")
+    
+  },Renv,FLenv)
+}
+)
+
+
+sqlQuery(connection,"CALL  FLRegrDataPrep('FL_DEMO.tblAbaloneWide', 
+ 'ObsID', 
+         'Diameter', 
+         'FL_DEMO.ARBasetblAbaloneWideD1469186546', 
+         'obs_id_colname', 
+         'var_id_colname', 
+         'cell_val_colname', 
+         0, 
+         0, 
+         0, 
+         0, 
+         0, 
+         1, 
+         0, 
+         'Sex,Num_Length,WholeWeight,VisceraWeight,Rings,DummyCat', 
+         NULL, 
+         NULL, 
+         NULL,AnalysisID)")
+
+
