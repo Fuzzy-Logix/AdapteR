83e5735981719eab65c7312e89919ad20e0ffdba
Gregor Kappler <g.kappler@gmx.net>
2016-08-08 14:42:11 +0200

    fixed step function

--- tests/testthat/test_step.R
+++ tests/testthat/test_step.R
@@ -1,26 +1,25 @@
-	connection <- flConnect(odbcSource = "Gandalf",database = "FL_TRAIN",platform="TD")
+## Since both the objects need to be dealt differently, objects have been initialized and 
+## defined outside test_that function
 
-	## Since both the objects need to be dealt differently, objects have been initialized and 
-	## defined outside test_that function
 
-	FLenv$stepobj <- FLTable("FL_DEMO", "tblAbaloneWide", "ObsID")
-	Renv$widetable<-as.data.frame(FLenv$stepobj)
+## Since FL and R environments have different case structure for column names
 
-	## Since FL and R environments have different case structure for column names
-	colnames(Renv$widetable)<-colnames(FLenv$stepobj)
 
-	Renv$stepobj<-lm(Diameter~Height+ShellWeight, data = Renv$widetable)
+## step function does not work properly. throws an error
+## asana ticket- https://app.asana.com/0/143316600934101/158092657328842
 
-	## step function does not work properly. throws an error
-	## asana ticket- https://app.asana.com/0/143316600934101/158092657328842
+options(debugSQL=T)
+test_that("test for step", {
+    flData <- FLTable("FL_DEMO", "tblAbaloneWide", "ObsID")
+    rData<-as.data.frame(flData)
+    colnames(rData)<-colnames(flData)
+    scope <- list(lower=Diameter~Height+ShellWeight,upper=Diameter~Height+ShellWeight+ShuckedWeight)
+    r <- step(lm(scope$upper, data = rData), scope = scope, direction = "backward")
+    fl <- step(flData, scope = scope, direction = "backward")
+    
+    expect_equal(coef(r),coef(fl))
 
-	test_that("test for step", {
-	  eval_expect_equal({
-	    
-	   r<-step(stepobj,scope = list(lower=Diameter~Height,upper=Diameter~Height+ShellWeight+ShuckedWeight), direction = "backward")
-	    
-	  },Renv,FLenv,
-	  )
-	  
-	}
-	)
+    fl <- step(flData, scope = scope, direction = "forward")
+    
+    expect_equal(coef(r),coef(fl))
+})
