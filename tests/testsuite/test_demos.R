086ec58713a219e82a4ad4276738593f387f31d0
Gregor Kappler <g.kappler@gmx.net>
2016-09-08 13:24:54 +0200

    demo tests, gam testing needs rework

--- tests/testsuite/test_demos.R
+++ tests/testsuite/test_demos.R
@@ -2,6 +2,7 @@ demos <- list.files(path="../demo",pattern=".*\\.R$",full.names = TRUE)
 oreadline <- readline
 for(f in demos){
     test_that(paste0("demo ", gsub("^.*/","",f), "runs"), {
+        cat(paste0("running demo ", gsub("^.*/","",f), "\n"))
         readline <- function(x) cat(paste0(x," \n"))
         source(f)
     })
