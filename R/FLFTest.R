599c2bf3c55733e3df9793eeac369436bdaa7bbc
Gregor Kappler <g.kappler@gmx.net>
2016-09-28 01:04:05 +0200

    f test documentation

--- R/FLFTest.R
+++ R/FLFTest.R
@@ -1,13 +1,15 @@
 NULL
+
+## gk: please add see also 
 #' F Test.
 #' 
 #'
 #' Performs an F test to compare the variances of two samples from normal populations.
-#'#' The null hypothesis is that the ratio of the variances of the populations from which
+#' The null hypothesis is that the ratio of the variances of the populations from which
 #' x and y were drawn, or in the data to which the linear models x and y were fitted,
 #' is equal to ratio.
-#' @param x:  FLVectors of data values
-#' @param y: FLVectors of data values
+#' @param x: FLVector of data values
+#' @param y: FLVector of data values
 #' @param alternative: a character string specifying the alternative hypothesis,
 #'                    must be one of '"two.sided"' (default), '"greater"' or
 #'                   '"less"'.
@@ -20,12 +22,7 @@ NULL
 #' x <- as.FLVector(rnorm(50, mean = 0, sd = 2))
 #' y <- as.FLVector(rnorm(30, mean = 1, sd = 1))
 #' var.test(x, y, "two.sided")
-#'
-#'
-#' 
-
-
-
+#' @export
 var.test.FLVector <- function(x, y, ratio = 1, alternative = "two.sided", conf.level = .95){
     if(!is.FLVector(x) || !is.FLVector(y))
         print("only takes FLVector")
@@ -44,6 +41,7 @@ var.test.FLVector <- function(x, y, ratio = 1, alternative = "two.sided", conf.l
         p <- createTable(tName,pSelect=t)
         vcall <- as.list(sys.call())
         dname <- paste0(vcall[[2]]," and ",vcall[[3]])
+        # gk: please use a generator function
         str <- paste0("WITH z(DataSetID, GroupID, Num_Val) AS
                                  (SELECT q.* FROM ",p," AS q)
 SELECT a.* FROM TABLE (FLFTestUdt(z.DataSetID, '",vmapping[alternative],"', z.GroupID, z.Num_Val)
