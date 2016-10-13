07ba7a897115ac10d9640659a0c3a7b1cdea1b70
Amal <amal.shrivastava@fuzzylogix.com>
2016-10-13 15:34:56 +0530

    cleaning directory

--- R/FLHypothesisTesting.R
+++ R/FLHypothesisTesting.R
@@ -1,11 +1,23 @@
 
-cint<-function(x,conf.level,alternative="two.sided"){
+cint<-function(x,y=NULL,mu=0,conf.level,alternative="two.sided"){
+    browser()
     if (alternative=="two.sided")
-    {
+    { if(length(y)==0){
         df<-length(x)-1
         sd<-sd(x)/sqrt(length(x))
         qt<-qt(conf.level+(1-conf.level)/2,df)*sd
-        res<-mean(x)+c(-qt,qt)
+        res<-mean(x)+c(-qt,qt)}
+      else{
+        stderx<-sqrt(var(x)/length(x))
+        stdery<-sqrt(var(y)/length(y))
+        stder<-sqrt(stderx^2+stdery^2)
+        df<-stder^4/(stderx^4/(length(x)-1) + stdery^4/(length(y)-1))
+        tstat<-(mean(x)-mean(y)-mu)/stder
+        pval <- 2 * pt(-abs(tstat), df)
+        alpha <- 1 - conf.level
+        res<- qt(1 - alpha/2, df)
+        res<- tstat + c(-res,res)
+      }
     }
     else stop("Not available for others")
     return(res)
