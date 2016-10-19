53a12a00e1128f1ffd98f9d5579af896c61c7aee
Amal <amal.shrivastava@fuzzylogix.com>
2016-10-19 14:58:26 +0530

    changes to FLrpart

--- R/FLrpart.R
+++ R/FLrpart.R
@@ -1,26 +1,19 @@
-FLrpart<-function(data=flt,
+FLrpart<-function(data,
 				  formula,
-				  control=list(minsplit=10,
+				  control=rpart.control(minsplit=10,
 				  maxdepth=5,
 				  cp=0.95),
 				  method="class",
 				  ...){
-	if(flt@isDeep){
+	if(!class(formula)=="formula") stop("Please enter a valid formula")
+	if(data@isDeep){
 		if(control["cp"]>1 || control["cp"]<0) stop("cp should be between 0 and 1")
 		if(!class(formula)=="formula") stop("Please enter a valid formula")
-		else{
-			if(!isDotFormula(formula)){
-				vcolnames<-colnames(flt)
-				vexclude<-setdiff(vcolnames,all.vars(formula))}
-			else{
-				stop("Still developing")
-			}
-		}
-		tablename<-flt@select@table_name
+		tablename<-data@select@table_name
 		deeptablename<-createView(pViewName=gen_view_name("New"),
-							  	  pSelect=constructSelect(flt))
+							  	  pSelect=constructSelect(data))
 		vnote<-genNote("abc")
-		vcolnames<- flt@select@variables
+		vcolnames<- data@select@variables
 		vinputcols<-c(INPUT_TABLE=deeptablename,
 					  OBSID="obs_id_colname",
 					  VARID="var_id_colname",
@@ -42,5 +35,11 @@ FLrpart<-function(data=flt,
 		return(ret)
 		
 	}
-		else stop("not available for wide tables yet")
+	else{
+		if(!isDotFormula(formula)){
+			vcolnames<-colnames(data)
+			vexclude<-setdiff(vcolnames,all.vars(formula))}
+		else{
+			vcolnames<-colnames(data)}
+	}	
 }
