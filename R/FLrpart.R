579addcdcd3b8a1e3f0dcdd76e1eba39b5002c57
Amal <amal.shrivastava@fuzzylogix.com>
2016-10-20 16:25:18 +0530

    rpart added for widetables

--- R/FLrpart.R
+++ R/FLrpart.R
@@ -10,36 +10,46 @@ FLrpart<-function(data,
 		if(control["cp"]>1 || control["cp"]<0) stop("cp should be between 0 and 1")
 		if(!class(formula)=="formula") stop("Please enter a valid formula")
 		tablename<-data@select@table_name
-		deeptablename<-createView(pViewName=gen_view_name("New"),
-							  	  pSelect=constructSelect(data))
-		vnote<-genNote("abc")
-		vcolnames<- data@select@variables
-		vinputcols<-c(INPUT_TABLE=deeptablename,
-					  OBSID="obs_id_colname",
-					  VARID="var_id_colname",
-					  VALUE="cell_val_colname",
-					  MINOBS=control["minsplit"],
-					  MAXLEVEL=control["maxdepth"],
-					  PURITY=control["cp"],
-					  NOTE=vnote)
-		vfuncName<-"FLDecisionTreeMN"
-		retobj<-sqlStoredProc(getFLConnection(),
-							  vfuncName,
-							  outputParameter=c(AnalysisID="a"),
-							  pInputParameters=vinputcols)
-	
-		AnalysisID<-as.character(retobj[1,1])
-
-		sql<-paste0("Select * from fzzlDecisionTreeMN where AnalysisID = ",fquote(AnalysisID)," Order by 1,2,3")
-		ret<-sqlQuery(connection,sql)
-		return(ret)
-		
+		deepx<-data
 	}
 	else{
 		if(!isDotFormula(formula)){
-			vcolnames<-colnames(data)
-			vexclude<-setdiff(vcolnames,all.vars(formula))}
+			data <- setAlias(data,"")
+			vcolnames<-colnames(data)		
+			vexclude<-setdiff(vcolnames,all.vars(formula))
+			obs<-getVariables(data)[["obs_id_colname"]]
+			vexclude<-setdiff(vexclude,obs)
+			vexclude<-paste0(vexclude,collapse=",")
+		}
 		else{
-			vcolnames<-colnames(data)}
-	}	
+			vexclude<-NULL
+		}
+			depCol<-all.vars(formula)[1]
+			deep<-FLRegrDataPrep(data,depCol=depCol,excludeCols=vexclude)
+			deepx<-deep[["table"]]
+			deepx<- setAlias(deepx,"")
+			deeptablename<-deepx@select@table_name
+	}
+	deeptablename<-createView(pViewName=gen_view_name("New"),
+							  	  pSelect=constructSelect(deepx))
+	vnote<-genNote("abc")
+	vcolnames<- deepx@select@variables
+	vinputcols<-c(INPUT_TABLE=deeptablename,
+				  OBSID="obs_id_colname",
+				  VARID="var_id_colname",
+				  VALUE="cell_val_colname",
+				  MINOBS=control["minsplit"],
+				  MAXLEVEL=control["maxdepth"],
+				  PURITY=control["cp"],
+				  NOTE=vnote)
+	vfuncName<-"FLDecisionTreeMN"
+	retobj<-sqlStoredProc(getFLConnection(),
+						  vfuncName,
+						  outputParameter=c(AnalysisID="a"),
+						  pInputParameters=vinputcols)
+
+	AnalysisID<-as.character(retobj[1,1])
+	sql<-paste0("Select * from fzzlDecisionTreeMN where AnalysisID = ",fquote(AnalysisID)," Order by 1,2,3")
+	ret<-sqlQuery(connection,sql)
+	return(ret)	
 }
