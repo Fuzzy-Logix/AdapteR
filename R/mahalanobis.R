4ac55752eec11d9814dfe8b4f2451ed21bd7dfbc
Amal <amal.shrivastava@fuzzylogix.com>
2016-12-06 18:34:18 +0530

    Documentation added for mahalanobis distance

--- R/mahalanobis.R
+++ R/mahalanobis.R
@@ -1,10 +1,29 @@
+#' Mahalanobis Distance
+#'
+#' Returns the squared Mahalanobis distance of an FLVector x and FLVector y with
+#' respect to a covariance matrix sigma = S.
+#'                          D^2 = (x - y)' Σ^-1 (x - y)
+#' 
+#' @param x An FLVector of data with say, p columns.
+#' @param y An FLVector of data with say, p columns.
+#' @param S A covariance matrix of the distribution(p*p)
+#' 
+#' @examples
+#' x<-FLVector(c(0,0))
+#' y<-FLVector(1:2)
+#' ma<-cbind(1:6,1:3)
+#' s<-var(ma)
+#' S<-as.FLMatrix(s)
+#' mahalanobis(x,y,S)
+#' @export
+
 mahalanobis<-function(x,y,S,...){
 	UseMethod("mahalanobis",x)
 }
 
 mahalanobis.default<-stats::mahalanobis
 
-mahalanobis.FLVector<-function(x,y,S,...){ #browser()
+mahalanobis.FLVector<-function(x,y,S,...){ browser()
 	#if(!class(x)||!class(y)=="FLVector") stop("Function only applies to numeric FLVectors of x and y")
 	#if(!class(S)=="FLMatrix") stop("Function works for a covariance matrix")
 
@@ -15,17 +34,17 @@ mahalanobis.FLVector<-function(x,y,S,...){ #browser()
                            					pID=1,
                            					pRowID=1,
                            					pColID="a.vectorIndexColumn",
-                           					pValue="a.vectorValueColumn"),
+                           					pValue="CAST(a.vectorValueColumn AS FLOAT)"),
                                         b=c(pGroupID=1,
                                         	pID=2,
                                         	pRowID=1,
                                         	pColID="b.vectorIndexColumn",
-                                        	pValue="b.vectorValueColumn"),
+                                        	pValue="CAST(b.vectorValueColumn AS FLOAT)"),
                                         c=c(pGroupID=1,
                                         	pID=0,
                                         	pRowID="c.rowIdColumn",
                                         	pColID="c.colIdColumn",
-                                        	pValue="c.valueColumn")))
+                                        	pValue="CAST(c.valueColumn AS FLOAT)")))
 
     p <- createTable(pTableName=gen_unique_table_name("temp"),pSelect=t,pTemporary=TRUE)
 	query<-constructUDTSQL(pViewColnames=c(pGroupID="pGroupID",pID="pID",pRowID="pRowID",pColID="pColID",pValue="pValue"),
