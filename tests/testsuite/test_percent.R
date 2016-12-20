b2a382e6206f52e5fd35027e24ef3a83b3fed760
phanisrikar93 <phanisrikar93ume@gmail.com>
2016-12-20 17:41:29 +0530

    fix: rank support on Hadoop: limitations documented on asana

--- tests/testsuite/test_percent.R
+++ tests/testsuite/test_percent.R
@@ -5,6 +5,8 @@ Renv$var2 = runif(10, 1, 10)
 
 FLenv = as.FL(Renv)
 
+## Fails in Hadoop: no index column in output
+## https://app.asana.com/0/150173007236461/233442378511381
 test_that("percent",{
           result = eval_expect_equal({
                    test1 = percent(var1)
