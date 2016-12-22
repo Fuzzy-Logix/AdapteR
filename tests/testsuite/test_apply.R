8c716783da5793c41314717b9fe17c68a9a2103d
Gregor Kappler <g.kappler@gmx.net>
2016-12-22 12:06:32 +0100

    apply:  fix ordering and dimnames

--- tests/testsuite/test_apply.R
+++ tests/testsuite/test_apply.R
@@ -7,10 +7,11 @@ FLenv <- as.FL(Renv)
 
 test_that("apply over FLMatrix", {
     result1=eval_expect_equal({
-        test1<-apply(m,2,mean)
-        test2 <- apply(m,1,max)
+        colmean <- apply(m,2,mean)
+        rowmean <- apply(m,1,mean)
+        rowsd <- apply(m,1,sd)
+        colsd <- apply(m,2,sd)
+        rowmax <- apply(m,1,max)
     },Renv,FLenv,
-    expectation=c("test1","test2"),
     tolerance=1e-6)
 })
-
