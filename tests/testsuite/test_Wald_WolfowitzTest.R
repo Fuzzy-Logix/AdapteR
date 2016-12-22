7e569f9b2530965bdb711cd28847f2b325ec541a
Gregor Kappler <g.kappler@gmx.net>
2016-12-22 13:42:10 +0100

    wald wolfowitz tests: 2 sample moved to upstream

--- tests/testsuite/test_Wald_WolfowitzTest.R
+++ tests/testsuite/test_Wald_WolfowitzTest.R
@@ -62,31 +62,3 @@ test_that("Wald-Wolfowitz two Sample Test:",{
     verbose=T,
     check.attributes=FALSE)
 })
-
-
-
-
-#Wald-Wolf Test 2s
-
-Renv = new.env(parent = globalenv())
-Renv$a = c(35,44,39,50,48,29,60,75,49,66)
-Renv$b = c(17, 23, 13, 24, 33, 21, 18, 16, 32)
-FLenv = as.FL(Renv)
-FLenv$res1 <- WaldWolftest2s(FLenv$a, FLenv$b)
-Renv$res1 <- runs.test(Renv$a, Renv$b, alternative = "two.sided")
-
-
-
-                                        # source: http://www.statext.com/practice/WaldWolfowitzRunsTestTwo02.php
-                                        #result of source gives Z = -2.83 which is same as computed by FL Test
-
-test_that("Wald-Wolfowitz two Sample Test:",{
-    result = eval_expect_equal({
-        p.val <- res1$p.value
-        z <- res1$statistic
-    },Renv,FLenv,
-    noexpectation=c("res1"),
-    expectation =c( "p.val", "z"),
-    verbose=T,
-    check.attributes=FALSE)
-})
