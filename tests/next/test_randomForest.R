b78069c584656c5970ed194a649312193954b825
Amal <amal.shrivastava@fuzzylogix.com>
2017-02-08 17:38:31 +0530

    New branch created. Added prediction test cases for rpart and randomforests

--- tests/next/test_randomForest.R
+++ tests/next/test_randomForest.R
@@ -5,6 +5,7 @@ FLenv$table<-FLTable("tblDecisionTreeMulti","ObsID","VarID","Num_Val")
 Renv$table<-as.data.frame(FLenv$table)
 Renv$table$`-1`<-as.factor(Renv$table$`-1`)
 colnames(Renv$table)<-paste0("Col",1:ncol(Renv$table))
+FLenv$newdata<-FLenv$table[1:150,]
 ntree<-5
 
 test_that("test for randomForests on deeptables",{
@@ -16,3 +17,14 @@ test_that("test for randomForests on deeptables",{
   result4= expect_equal(length(flobj$predicted),length(robj$predicted))
   result5= expect_equal(sum(flobj$confusion),length(robj$y))
 })
+
+test_that("test for predict in randomForest",{
+  flobj<-randomForest(FLenv$table, formula = -1~.,ntree=ntree)
+  flobj1<-predict(flobj, FLenv$newdata)
+  flobj2<-predict(flobj, FLenv$newdata, type = "prob")
+  result1 = expect_equal(length(flobj1), nrow(FLenv$newdata))
+  result2 = expect_equal(flobj$classes, unique(as.vector(flobj1)))
+  result3 = expect_equal(length(flobj2), nrow(FLenv$newdata))
+  result4 = expect_equal(FALSE, any(as.vector(flobj2>1)))
+})
+
--- tests/testthat/test_rpart.R
+++ tests/testthat/test_rpart.R
@@ -4,7 +4,10 @@ FLenv=as.FL(Renv)
 FLenv$table<-FLTable("tblDecisionTreeMulti","ObsID","VarID","Num_Val")
 Renv$table<-as.data.frame(FLenv$table)
 Renv$table$`-1`<-as.factor(Renv$table$`-1`)
+FLenv$newdata<-FLenv$table[1:150,]
 colnames(Renv$table)<-paste0("Col",1:ncol(Renv$table))
+
+print(methods("rpart"))
 
 test_that("test for decision tree on deep tables",{
   flobj<-rpart(FLenv$table, formula = -1~.)
@@ -12,4 +15,13 @@ test_that("test for decision tree on deep tables",{
   result1= expect_equal(flobj$frame[1,"n"],robj$frame[1,"n"])
   result3= expect_equal(as.numeric(rownames(flobj$frame)),as.numeric(flobj$frame$NodeID))
   result5= expect_equal(any(flobj$frame$var=="<leaf>"),TRUE)
+})
+
+test_that("test for prediction in rpart",{
+  flobj<-rpart(FLenv$table, formula = -1~.)
+  flobj1<-predict(flobj, FLenv$newdata)
+  flobj2<-predict(flobj, FLenv$newdata, type = "prob")
+  result1 = expect_equal(length(flobj1), nrow(FLenv$newdata))
+  result2 = expect_equal(length(flobj2), nrow(FLenv$newdata))
+  result3 = expect_equal(FALSE, any(as.vector(flobj2>1)))
 })
