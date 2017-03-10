cd5a662f84413ac62268fc5c1a7917087f791ba9
Sanchit <chiplunkarsanchit1@gmail.com>
2017-03-10 14:45:31 +0530

    demo changes

--- demo/CreditcardDemo.R
+++ demo/CreditcardDemo.R
@@ -13,7 +13,7 @@ vSampleDataTables <- suppressWarnings(SampleData(pTableName="ARcreditcard",
                                   pObsIDColumn="ObsID",
                                   pTrainTableName="ARcreditcardTrain",
                                   pTestTableName="ARcreditcardTest",
-                                  pTrainDataRatio=.2,
+                                  pTrainDataRatio=.05,
                                   pTemporary=FALSE,
                                   pDrop=TRUE))
 vTrainTableName <- vSampleDataTables["TrainTableName"]
@@ -29,8 +29,9 @@ dropTable(deepTableName)
 FLtbl <- FLTable(vTrainTableName,"ObsID",fetchIDs=FALSE)
 FLTestTbl <- FLTable(vTestTableName,"ObsID",fetchIDs=FALSE)
 
-myformula <- eval(parse(text=paste0(vdependentColumn,"~.")))
+vdependentColumn <- "Classvar"
 
+myformula <- eval(parse(text=paste0(vdependentColumn,"~.")))
 if(!existsRemoteTable(tableName=deepTableName)){
     FLdeepTable <- prepareData(formula         = myformula ,
                                data            = FLtbl,
@@ -62,35 +63,36 @@ plot2.FLROC(glm.roc, limit = 1000, main = "glm-roc")
 ## Decision Tree.
 ## change purity level  -> .999
 dt.model <- rpart(myformula,data = FLdeepTable, control = c(minsplit = 15, cp = .9999, maxdepth = 10))
-odt.predict <- predict(dt.model,type = "prob")
+dt.predict <- predict(dt.model,type = "prob")
 length(dt.predict)
 dt.roc <- roc.FLVector(FLtbl$Classvar, dt.predict)
 plot(dt.roc, limit = 1000, main = "dt-roc", method = 0)
 
-
-## Random Forest:
-rf.model <- randomForest.FLTable(Classvar ~ ., data = FLtbl, minsplit = 15, cp = .9999, maxdepth = 7)
-rf.predict <- predict(rf.model,type = "prob")
-length(dt.predict)
-rf.roc <- roc.FLVector(FLtbl$Classvar, rf.predict)
-plot.FLROC(rf.roc, limit = 1000, main = "rf-roc", method = 0)
-
-
-
-
-
-## combined plot:
-png("combined-plot1.png")
-par(mfrow = c(2, 1))
-##rf.plot <- plot(rf.roc, limit = 1000, main = "rf-roc")
-##ch <- paste0("auc of ",round(rf.plot$auc, digits=3))
+##
+#### Random Forest:
+##rf.model <- randomForest(myformula,  data = FLdeepTable, minsplit = 15, cp = .9999, maxdepth = 7)
+##rf.predict <- predict(rf.model,type = "prob")
+##length(rf.predict)
+##rf.roc <- roc.FLVector(FLtbl$Classvar, rf.predict)
+##plot.FLROC(rf.roc, limit = 1000, main = "rf-roc", method = 0)
+##
+##
+##
+##
+##
+#### combined plot:
+##png("combined-plot1.png")
+##par(mfrow = c(2, 1))
+####rf.plot <- plot(rf.roc, limit = 1000, main = "rf-roc")
+####ch <- paste0("auc of ",round(rf.plot$auc, digits=3))
+####mtext(ch, side = 3)
+##dt.plot <- plot(dt.roc, limit = 1000, main = "dt-roc", method = 0)
+##ch <- paste0("auc of ",round(dt.plot$auc, digits=3))
 ##mtext(ch, side = 3)
-dt.plot <- plot(dt.roc, limit = 1000, main = "dt-roc", method = 0)
-ch <- paste0("auc of ",round(dt.plot$auc, digits=3))
-mtext(ch, side = 3)
-glm.plot <- plot(glm.roc, limit = 1000, main = "glm-roc")
-ch <- paste0("auc of ",round(glm.plot$auc, digits=3))
-mtext(ch, side = 3)
-dev.off()
-
-   
+##glm.plot <- plot(glm.roc, limit = 1000, main = "glm-roc")
+##ch <- paste0("auc of ",round(glm.plot$auc, digits=3))
+##mtext(ch, side = 3)
+##dev.off()
+##
+##   
+##
