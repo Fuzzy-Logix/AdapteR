0fadd7a533ec9045ba31f993d09c076478533a4d
Sanchit <chiplunkarsanchit1@gmail.com>
2017-03-27 14:42:26 +0530

    minor changes for svm test case

--- tests/next/test_SVM.R
+++ tests/next/test_SVM.R
@@ -1,4 +1,5 @@
 ## using Vectors.
+## For Linear Kernel.
 FLenv <- new.env(parent = globalenv())
 FLenv$tbl  <- FLTable("tblSVMLinSepMultiDim", "OBSID", whereconditions= "OBSID>307")
 Renv <- as.R(FLenv)
@@ -25,16 +26,19 @@ expectations = c("deg", "cost"))
 })
 
 
+## test will fail as some values depends aren't constant.
+test_that("",{
 expect_equal(FLenv$mod$misclassifications,5 )
 expect_equal(FLenv$mod$BValue,"Not applicable for this kernel")
 expect_equal(FLenv$mod$crbfConstant,"Not applicable for this kernel")
 expect_equal(FLenv$mod$lambda,"Not applicable for this kernel" )
-
+})
 
 
 
 
 ## Using formula
+## For Polynomial kernel.
 FLenv <- new.env(parent = globalenv())
 
 FLenv$tbl <- FLTable("tblSVMDense", "OBSID", whereconditions = "OBSID<307")
@@ -59,7 +63,12 @@ check.attributes = FALSE,
 expectations = c("deg", "cost"))
 })
 
+
+## test will fail as some values depends aren't constant.
+## misclass, Bvalue:
+test_that("", {
 expect_equal(FLenv$mod$misclassifications,8 )
 expect_equal(FLenv$mod$BValue,-6.79, tolerance = .0002)
 expect_equal(FLenv$mod$crbfConstant,"Not applicable for this kernel")
 expect_equal(FLenv$mod$lambda,.219, tolerance = .002)
+})
