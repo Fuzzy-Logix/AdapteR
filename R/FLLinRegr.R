1052bf864e12b58d803ed500baaea01eb0cceb4b
Gregor Kappler <g.kappler@gmx.net>
2017-03-31 14:10:23 +0200

    names.FLLinRegr added

--- R/FLLinRegr.R
+++ R/FLLinRegr.R
@@ -1406,6 +1406,30 @@ prepareData.lmGeneric <- prepareData.formula
 #' @export
 prepareData.NULL <- prepareData.formula
 
+setMethod("names",signature("FLLinRegr"),
+          function(x) c("coefficients",
+                        "residuals",
+                        "fitted.values",
+                        "FLCoeffStdErr",
+                        "FLCoeffTStat",
+                        "FLCoeffPValue",
+                        "FLCoeffNonZeroDensity",
+                        "FLCoeffCorrelWithRes",
+                        "s",
+                        "call",
+                        "FLLinRegrStats",
+                        "df.residual",
+                        "model",
+                        "x",
+                        "y",
+                        "qr",
+                        "rank",
+                        "terms",
+                        "xlevels",
+                        "assign",
+                        "formula",
+                        "anova"
+                        ))                        
 
 ## move to file lm.R
 #' @export
@@ -1940,11 +1964,6 @@ summary.FLLinRegr <- function(object,
 }
 
 
-
-## Amal: If this generic gets defined here, predict doesn't work for rpart
-#predict<-function(object,newdata,...){
-#	UseMethod("predict",object)
-#}
 
 ## move to file lm.R
 ## Add deep statment, also problem can be of vobsid
