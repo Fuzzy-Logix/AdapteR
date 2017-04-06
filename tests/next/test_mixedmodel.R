41f63a7135c0f5b1b4c10245a7dfdb95a5e53648
Sanchit <chiplunkarsanchit1@gmail.com>
2017-04-06 12:29:11 +0530

    test case for FLLinMixedmodel

--- tests/next/test_mixedmodel.R
+++ tests/next/test_mixedmodel.R
@@ -1,5 +1,4 @@
 rm(list = setdiff(ls(),"connection"))
-
 ## implement as.R for FLSimpleVector.
 ## using 1 Random Effects.
 FLenv <- new.env(parent = globalenv())
@@ -7,6 +6,7 @@ fltbl  <- FLTable("tblMixedModel", "ObsID")
 Renv <- as.R(FLenv)
 rtbl <- as.R(fltbl)
 
+## use in 
 FLenv$mod <- lmer(yVal ~ FixVal + (1 | RanVal), data = fltbl)
 Renv$mod <- lmer(yVal ~ FixVal + (1 | RanVal), data = rtbl, REML = FALSE)
  
@@ -51,18 +51,15 @@ test_that("Coeff of Random Effect", {
     expect_equal(FLenv$mod$u,Renv$mod@u,tolerance = .001 )
 })
 
+
 ## dblytix Example:
 FLenv <- new.env(parent = globalenv())
 fltbl  <- FLTable("tblLinMixedModelWide2", "ObsID")
 Renv <- as.R(FLenv)
 rtbl <- as.R(fltbl)
 
-
-FLenv$mod <- lmer(MathAch ~ CSes +(1 | School), data = fltbl)
+FLenv$mod <- lmer(MathAch ~ CSes +(1 | School), maxiter = 100, data = fltbl)
 Renv$mod <- lmer(MathAch ~ CSes +(1 | School), data = rtbl, REML = FALSE)
-
-
-
 
 ## AIC, Log-Likehhood
 test_that("AIC, LogLik:", {eval_expect_equal({
@@ -95,10 +92,7 @@ check.attributes = FALSE,
 expectations = c("vpred"))
 })
 
-## Covar Random
-test_that("", {
-    expect_equal(FLenv$mod$CovarRandom,260.573310,tolerance = .001 )
-})
+
 
 ## Coeff of Random Effect
 test_that("Coeff of Random Effect", {
