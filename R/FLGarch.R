2eca0174237a0f7c03c40a16d248ac42bf9523d3
Sanchit <chiplunkarsanchit1@gmail.com>
2017-04-28 20:13:21 +0530

    time series function PP function giving error

---
+++ R/FLGarch.R
@@ -0,0 +1,71 @@
+#' @export
+setClass("FLGarch",
+         slots = list(results = "list"))
+
+#' @export
+garch <- function (data=list(),degree, ...) {
+    UseMethod("garch", data)
+}
+
+
+##Example:
+#'rv <- sqlQuery(connection, "SELECT stockreturn FROM tblbac_return")
+#'flv <- as.FL(rv$stockreturn)
+#' garch(flv)
+#' @export
+garch.FLVector <- function(data,degree = 2, ...)
+{
+    if(!is.FLVector(data)){
+        stop("only applicable on FLVector")
+    }
+    browser()
+    functionName <- "FLIGarchUdt"
+    ##pArg <- c(pD = degree)
+    str <- constructUDTSQL(pViewColname = c(GroupID = 1,
+                                            q = 1,
+                                            p = 1,
+                                            Val = "vectorValuecolumn"),
+                         pFuncName = functionName,
+                         pOutColnames = c("a.*"),
+                         pSelect = constructSelect(data),
+                         ##pArgs = pArg,
+                         pLocalOrderBy=c("GroupID", "val"), pNest = TRUE, pFromTableFlag = FALSE)
+    vdf <- sqlQuery(connection, str)
+    return(vdf)
+}
+
+
+#' @export
+regimeshift <- function (data=list(),regimes = 2, ...) {
+    UseMethod("regimeshift", data)
+}
+
+
+##Example:
+#'vdf <-  sqlQuery(connection, "SELECT Num_Val FROM tblRegimeShift WHERE Groupid = 1 AND Obsid <500")
+#'flv <- as.FL(rv$Num_Val)
+#' regimeshift(flv)
+#' @export
+regimeshift.FLVector <- function(data,regimes = 2, ...)
+{
+    if(!is.FLVector(data)){
+        stop("only applicable on FLVector")
+    }
+##    browser()
+    functionName <- "FLRegimeShiftUdt"
+    pArg <- c(regimes = regimes)
+    str <- constructUDTSQL(pViewColname = c(GroupID = 1,
+                                            Val = "vectorValuecolumn"),
+                         pFuncName = functionName,
+                         pOutColnames = c("a.*"),
+                         pSelect = constructSelect(data),
+                         pArgs = pArg,
+                         pLocalOrderBy=c("GroupID"), pNest = TRUE, pFromTableFlag = FALSE)
+    vdf <- sqlQuery(connection, str)
+    vdf <- vdf[,2:length(vdf)]
+    names(vdf) <- c("Regime", "Mean", "StdDev", "Prob")
+               
+    return(vdf)
+}
+
+
---
+++ R/FLPP.R
@@ -0,0 +1,65 @@
+#' vdf <- sqlQuery(connection, "SELECT Num_Val FROM tblsensex")
+#' flv <- as.FL(vdf$Num_Val)
+#' rv <- vdf$Num_Val
+#' flmod <- PP.test(flv)
+#' 
+
+
+#' @export
+PP.test<-function(object,...){
+	UseMethod("PP.test",object)
+}
+
+#' @export
+PP.test.default<-function (object,...) {
+     return(PP.test(object,...))
+}
+
+#' @bexport
+PP.test.FLTable<-function(object,
+                           trend=1,
+                           lag = 1,
+                           ...){
+    browser()
+    ##if(!is.FLVector(object)) stop("The class of the object should be FLVector.")
+    vinputcols<-list(TableName=getTableNameSlot(object),
+                     ObsIDCol=object@select@variables$obs_id_colname,
+                     ValueCol="NUM_VAL",
+                     Trend=trend,
+                     Lag=lag)
+    vfuncName<-"FLPP"
+    dname <- as.character(match.call())[[2]]
+    AnalysisID<-sqlStoredProc(getFLConnection(),
+                              vfuncName,
+                              outputParameter=c(AnalysisID="a"),
+                              pInputParameters=vinputcols)
+    ret<-sqlQuery(getFLConnection(),paste0("select coeff, stderr, PVal_ZTAlpha as Pval,Z_T_Alpha as dft from fzzlPPStats where AnalysisID = ", fquote(AnalysisID[1,1])))
+    ##statistic<-ret[1,4]
+    ##parameter<-k
+    statistic = ret$dft
+    parameter=2
+    names(statistic)<-"Dickey-Fuller"
+    names(parameter)<-"Truncation lag parameter"
+    vreturn <- structure(list(statistic = statistic,
+                          parameter=parameter,
+                          ##alternative="stationary",
+                          p.value=ret$Pval,
+                          method="Phillips-Perron Unit Root Test",
+                          data.name=dname),
+                     class  = "htest"
+                     )    
+    return(vreturn)   
+}
+
+`$.htest`<-function(object,property){
+    parentObject <- unlist(strsplit(unlist(strsplit(as.character(sys.call()),"(",fixed=T))[2],",",fixed=T))[1]
+
+    if(property == "statistic"){
+        return(object$dft)}
+    if(property == "method"){
+        return("Phillips-Perron Unit Root Test")}
+    if(property == "parameter"){
+        return(0)}
+    if(property == "p.value"){
+        return(object$Pval)      
+} }
