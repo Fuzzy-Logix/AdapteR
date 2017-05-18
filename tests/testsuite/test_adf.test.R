0b7a4b2e7ace57ccc47717ab5b6c1be4dc5e285d
mohakuma <mohakgrg94@gmail.com>
2017-05-18 18:11:15 +0530

    changed value of k to 7

--- tests/testsuite/test_adf.test.R
+++ tests/testsuite/test_adf.test.R
@@ -1,8 +1,8 @@
 Renv=new.env(parent=globalenv())
 Renv$x<-rnorm(1000)
 FLenv<-as.FL(Renv)
-Renv$k<-8
-FLenv$k <- 8
+Renv$k<-7 
+FLenv$k <- 7
 
 ## fails presently. Need to figure out why.
 test_that("test for Augmented Dickey Fuller test",{
