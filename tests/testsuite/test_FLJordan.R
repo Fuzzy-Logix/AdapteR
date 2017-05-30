daa86732d91c26115d975ad47f7a980a98b01c85
mohakuma <mohakgrg94@gmail.com>
2017-05-30 11:47:40 +0530

    assigned objects in testcases which were earlier printed

--- tests/testsuite/test_FLJordan.R
+++ tests/testsuite/test_FLJordan.R
@@ -1,6 +1,11 @@
 ###########################
 ## DBLytix check correctness of decomposition, flm= P*J*PInv
-flm<- FLMatrix(getTestTableName("tblMatrixMulti"), 5,"MATRIX_ID","ROW_ID","COL_ID","CELL_VAL")
+flm<- FLMatrix(getTestTableName("tblMatrixMulti"), 5,
+				"MATRIX_ID",
+				"ROW_ID",
+				"COL_ID",
+				"CELL_VAL", 
+				dims= c(5,5))
 
 test_that("FLJordan(flm): FLJordan(flm)$P %*% diag(FLJordan(flm)$J) %*% FLJordan(flm)$PInv == flm",{
 	FLResultList <- FLJordan(flm)
@@ -16,9 +21,10 @@ test_that("Testing if basic DBLytix FLJordanDecompUdt-FLJordan Example runs from
 						 "MATRIX_ID",
 						 "ROW_ID",
 						 "COL_ID",
-						 "CELL_VAL")
+						 "CELL_VAL",
+						 dims=c(5,5))
 	resultList <- FLJordan(flmatrix)
-	resultList$J
-	resultList$P
-	resultList$PInv
+	JMatrix <- as.matrix(resultList$J)
+	PMatrix <- as.matrix(resultList$P)
+	PInvMatrix <- as.matrix(resultList$PInv)
 })
