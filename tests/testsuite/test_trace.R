56b99a1c3bc6819a04a77c5f524b430ff8606f56
mohakuma <mohakgrg94@gmail.com>
2017-06-01 01:35:28 +0530

    few test cases added in test_files and links in documentation

--- tests/testsuite/test_trace.R
+++ tests/testsuite/test_trace.R
@@ -1,5 +1,16 @@
+##Testing FLTrace
+Renv = new.env(parent = globalenv())
+Renv$mat1 = cbind(1, 1:3, c(2,1,3))
+FLenv <- as.FL(Renv)
 
-## Testing FLTrace
+test_that("Check for trace function ",{
+    result = eval_expect_equal({
+        e <- tr(mat1)
+    }, 	Renv,FLenv,
+    	expectation= c("e"))
+})
+
+## Testing FLTrace, initF based test
 test_that("check FLTrace",
 {
     expect_eval_equal(initF.FLMatrix,
@@ -7,4 +18,16 @@ test_that("check FLTrace",
                     psych::tr,
                     n=5,
                     isSquare=TRUE)
+})
+
+####################
+## DBLytix Example
+test_that("Testing if basic DBLytix FLMatrixTraceUdt-solve Example runs from AdapteR",{
+	FLMatrixObj <- FLMatrix(getTestTableName("tblmatrixmulti"),5,
+							"matrix_id",
+							"row_id",
+							"col_id",
+							"cell_val",
+							dims= c(5,5))
+	ResultFLVector <- tr(FLMatrixObj)
 })
