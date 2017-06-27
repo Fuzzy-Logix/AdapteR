7a6c8cd8d6265bc7c3121f2a2d3e92fa0210a032
vikalp261 <vikalp261@yahoo.in>
2017-06-27 12:44:43 +0530

    Fixed the problem with mixUDT's and lmer's documentation

--- R/FLMixedModelUdt.R
+++ R/FLMixedModelUdt.R
@@ -15,19 +15,17 @@ setClass(
 
 
 ## TODO :- implement storage in results slot.
-
-#' @export
-mixUDT <- function (formula,data=list(),...) {
-    UseMethod("mixUDT", data)
-}
-
-#' \code{mixudt} performs mixed model  on FLTable objects.
+#' Mixed Model
+#'
+#' \code{mixUDT} performs mixed model on FLTable objects.
+#'
 #' The DB Lytix function called is FLMixedModelUdt or FLMixedModelIntUdt.
 #' The mixed model extends the linear model by allowing correlation and
 #' heterogeneous variances in the covariance matrix of the residuals.
 #' Fita a linear mixed-effects model (LMM) to data,
 #' FLMixedModelUdt and FLMixedModelIntUdt estimates the coefficients and covariance
 #' matrix of the mixed model via expectation maximization method.
+#'
 #' @seealso \code{\link[lme4]{lmer}} for R reference implementation.
 #' @param formula A symbolic description of model to be fitted
 #' @param data An object of class FLTable.
@@ -44,13 +42,19 @@ mixUDT <- function (formula,data=list(),...) {
 #' @return \code{lmer} returns an object of class \code{FLMixUDT}
 #' @examples
 #' One Random Effect.
-#' fltbl  <- FLTable("tblMixedModel", "ObsID")
+#' fltbl  <- FLTable(getTestTableName("tblMixedModel"), "ObsID")
 #' flmod <- mixUDT(yVal ~ FixVal + (1 | RanVal), data = fltbl, pIntercept = 1)
 #' flpred <- predict(flmod)
 #' 2 Random Effects.
-#' tbl  <- FLTable("tblMixedModelInt", "ObsID")
+#' tbl  <- FLTable(getTestTableName("tblMixedModelInt"), "ObsID")
 #' flmod <- mixUDT(yVal ~ FixVal + (1 |   RanVal1) + (1 | RanVal2 ), tbl)
 #' flpred <- predict(flmod)
+#' @export
+mixUDT <- function (formula,data=list(),...) {
+    UseMethod("mixUDT", data)
+}
+
+
 #' @export
 mixUDT.FLTable <- function(formula, data, fetchID = TRUE,...)
 {
