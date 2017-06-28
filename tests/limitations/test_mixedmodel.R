3cbcc1d74a95a6f222daaec639d2e50bbe7153fa
vikalp261 <vikalp261@yahoo.in>
2017-06-28 15:10:38 +0530

    Testing : mixUDT and lmer

---
+++ tests/limitations/test_mixedmodel.R
@@ -0,0 +1,104 @@
+## Asana Task: https://app.asana.com/0/136555696724838/374759104097006/f
+## Results do not match with R's lmer
+
+
+rm(list = setdiff(ls(),"connection"))
+## implement as.R for FLSimpleVector.
+## using 1 Random Effects.
+FLenv <- new.env(parent = globalenv())
+fltbl  <- FLTable(getTestTableName("tblMixedModel"), "ObsID")
+Renv <- as.R(FLenv)
+rtbl <- as.R(fltbl)
+
+## use in testthat.
+FLenv$mod <- lmer(yVal ~ FixVal + (1 | RanVal), data = fltbl)
+Renv$mod <- lmer(yVal ~ FixVal + (1 | RanVal), data = rtbl, REML = FALSE)
+ 
+## AIC, Log-Likehhood
+test_that("AIC, LogLik:", {eval_expect_equal({
+    vAkaike <- AIC(mod)
+    vLik <- logLik(mod)
+    
+},Renv,FLenv,
+verbose = TRUE,
+check.attributes = FALSE,
+expectations = c("vAkaike", "vLik"))
+})
+
+## predict
+test_that("predict:", {eval_expect_equal({
+    vpred <- predict(mod)
+    
+},Renv,FLenv,
+verbose = TRUE,
+check.attributes = FALSE,
+expectations = c("vpred"))
+})
+
+## residuals.
+test_that("residuals:", {eval_expect_equal({
+    vres <- residuals(mod)
+    
+},Renv,FLenv,
+verbose = TRUE,
+check.attributes = FALSE,
+expectations = c("vpred"))
+})
+
+## Covar Random
+test_that("", {
+    expect_equal(FLenv$mod$CovarRandom,260.573310,tolerance = .001 )
+})
+
+## Coeff of Random Effect
+test_that("Coeff of Random Effect", {
+    expect_equal(FLenv$mod$u,Renv$mod@u,tolerance = .001 )
+})
+
+
+
+
+## dblytix Example:
+FLenv <- new.env(parent = globalenv())
+fltbl  <- FLTable(getTestTableName("tblLinMixedModelWide2"), "ObsID")
+Renv <- as.R(FLenv)
+rtbl <- as.R(fltbl)
+
+FLenv$mod <- lmer(MathAch ~ CSes +(1 | School), maxiter = 100, data = fltbl)
+Renv$mod <- lmer(MathAch ~ CSes +(1 | School), data = rtbl, REML = FALSE)
+
+## AIC, Log-Likehhood
+test_that("AIC, LogLik:", {eval_expect_equal({
+    vAkaike <- AIC(mod)
+    vLik <- logLik(mod)
+    
+},Renv,FLenv,
+verbose = TRUE,
+check.attributes = FALSE,
+expectations = c("vAkaike", "vLik"))
+})
+
+## predict
+test_that("predict:", {eval_expect_equal({
+    vpred <- predict(mod)
+    
+},Renv,FLenv,
+verbose = TRUE,
+check.attributes = FALSE,
+expectations = c("vpred"))
+})
+
+## residuals.
+test_that("residuals:", {eval_expect_equal({
+    vres <- residuals(mod)
+    
+},Renv,FLenv,
+verbose = TRUE,
+check.attributes = FALSE,
+expectations = c("vpred"))
+})
+
+## Coeff of Random Effect
+test_that("Coeff of Random Effect", {
+    expect_equal(FLenv$mod$u,Renv$mod@u,tolerance = .001 )
+})
--- tests/testthat/test_mixudt.R
+++ tests/testthat/test_mixudt.R
@@ -2,26 +2,33 @@ rm(list = setdiff(ls(),"connection"))
 ## implement as.R for FLSimpleVector.
 ## using 1 Random Effects.
 FLenv <- new.env(parent = globalenv())
-fltbl  <- FLTable("tblMixedModel", "ObsID")
+fltbl  <- FLTable(getTestTableName("tblMixedModel"), "ObsID")
 
 ## use in 
 FLenv$mod <- mixUDT(yVal ~ FixVal + (1 | RanVal), data = fltbl)
 
+
 ## AIC, Log-Likehhood
-test_that("AIC, LogLik:", {expect_equal(FLenv$mod$logLik,6970.309,tolerance = .001 )
-    expect_equal(FLenv$mod$AIC,6974.309,tolerance = .001 )  })
+test_that("AIC, LogLik:", {
+	expect_equal(FLenv$mod$logLik,6970.309,tolerance = .001 )
+    expect_equal(FLenv$mod$AIC,6974.309,tolerance = .001 )  
+})
 
 ## Covar Random
-test_that("", {
+test_that("Covar Random", {
     expect_equal(FLenv$mod$CovRandom,260.573310,tolerance = .001 )
 })
 
-
+## fixedcoef and u
+test_that("comparing length of fixed coef and u", {
+	expect_equal(1 + length(FLenv$mod$fixedcoef),length(unique(as.vector(fltbl[,"FixVal"]))))
+	expect_equal(length(FLenv$mod$u),length(unique(as.vector(fltbl[,"RanVal"]))))
+})
 
 
 ## for 2 Random Effect:
 FLenv <- new.env(parent = globalenv())
-fltbl  <- FLTable("tblMixedModelInt", "ObsID")
+fltbl  <- FLTable(getTestTableName("tblMixedModelInt"), "ObsID")
 
 FLenv$mod <- mixUDT(yVal ~ FixVal + (1 |   RanVal1) + (1 | RanVal2 ), fltbl)
 
@@ -30,6 +37,6 @@ test_that("AIC, LogLik:", {expect_equal(FLenv$mod$logLik,38.6181,tolerance = .00
     expect_equal(FLenv$mod$AIC,42.6181,tolerance = .001 )  })
 
 ## Covar Random
-test_that("", {
+test_that("Covar Random", {
     expect_equal(FLenv$mod$CovRandom,c(0.01482593 ,2.38859471),tolerance = .001 )
 })
