3e755e2059f3d2aa428fce29c368342fc9b71d4d
mohakuma <mohakgrg94@gmail.com>
2017-07-06 15:01:42 +0530

    added testcase of chi square

--- tests/next/test_chisq.test.R
+++ tests/next/test_chisq.test.R
@@ -22,3 +22,19 @@ test_that("test for pearson chi square on a matrix",{
 	FLexpect_equal(as.numeric(r[["p.value"]]),
                 as.numeric(fl[["p.value"]]))
 	})
+
+test_that("test for chi square on a matrix",{
+	fl <- chisq.test(FLenv$M)
+	chi_sq <- data.frame(c(4.834967, 6.273369), c(0.1692254, 0.2195700),
+						c(8.084012, 10.489006))
+	colnames(chi_sq) <- c("Democrat", "Independent", "Republican")
+	rownames(chi_sq) <- c("F", "M")
+
+	exp_val <- data.frame(c(703.6714,  542.3286), c(319.6453, 246.3547),
+						c(533.6834, 411.3166))
+	colnames(exp_val) <- c("Democrat", "Independent", "Republican")
+	rownames(exp_val) <- c("F", "M")
+
+	expect_equal(fl$chi_sq, chi_sq, tolerance= 1e-5)
+	expect_equal(fl$exp_val, exp_val, tolerance= 1e-5)
+	})
