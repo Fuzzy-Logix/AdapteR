6d37d1a8101a081f5ed47c719e04714afd0c585d
Sanchit <chiplunkasanchit1@gmail.com>
2017-07-06 17:15:27 +0530

    FLWOE, InfoVal implemement

--- R/FLWOE.R
+++ R/FLWOE.R
@@ -3,6 +3,11 @@ setClass("FLWOE",
          slots = list(table = "FLTable",
                       results = "list") )
 
+#' @export
+setClass("FLInfoVal",
+         slots = list(table = "FLTable",
+                      results = "list") )
+
 #' \code{woe} Performs woe on FLTable objects.It is an aggregate function which
 #' Calculates the statistical Weight of Evidence (WOE). WOE is a measure of how much
 #' an attribute in the data is related to statuses. This analysis can be used to
@@ -12,7 +17,7 @@ setClass("FLWOE",
 #' @param nonevents Non-Events or element of bad distribution.
 #' @param n number of ID's for which information is to be calculated.
 #' fltbl <- FLTable(table = "tblinfoval", obs_id_colname="BinID")
-#' flmod <- woe.FLTable(event = "Events", nonevents = "NonEvents", data = fltbl,n = 5)
+#' flmod <- woe.FLTable(event = "vEvents", nonevents = "vNonEvents", data = fltbl,n = 4)
 #' TO-DO: find examples and R implementation.
 #' @export
 woe <- function (events,nonevents,n = 2, data=list(),...) {
@@ -29,20 +34,90 @@ woe.FLTable <- function(events,nonevents,n = 2, data, ...)
     ##    browser()
     vcallObject <- match.call()
     vtbl <- data@select@table_name
-    vfun <- "FLWOE"    
-    vquery <- paste0("SELECT b.SerialVal AS BinID, FLWOE(a.BinID, a.",events,", a.",nonevents,", b.SerialVal) AS WOE FROM ",vtbl," a, fzzlSerial b WHERE b.SerialVal <= ",n," GROUP BY b.SerialVal ORDER BY 1;")
+    vfun <- "FLWOE"
+    vstr <- vQuery(vtbl, events, nonevents,n, vfun)
+
     vclass <- "FLWOE"
-    vdf <- sqlQuery(connection, vquery)
+    vdf <- sqlQuery(connection, vstr)
     ##print(vdf)
     return(new(vclass,
                table = data,
                results = list(otable = vdf))) }
 
+
+#' @export
+InfoVal.FLTable <- function(events,nonevents,n = 2, data, ...)
+{
+    ##    browser()
+    vcallObject <- match.call()
+    vtbl <- data@select@table_name
+    vfun <- "FLInfoVal"
+    vstr <- vQuery(vtbl, events, nonevents,n, vfun)
+    vclass <- "FLInfoVal"
+    vdf <- sqlQuery(connection, vstr)
+    ##print(vdf)
+    return(new(vclass,
+               table = data,
+               results = list(otable = vdf))) }
+
+
+
+vQuery <- function(table_name,events,nonevents,n,fName = "FLWOE",...){
+    if(fName == "FLWOE"){
+        var <- "b.SerialVal"
+        varname <- "WOE"
+    }
+    else{
+        var <- "b.SerialVal - 1"
+        varname <- "InfoVal"
+    }
+    
+    vquery <- paste0("SELECT ",var," AS BinID, ",fName,"(a.BinID, a.",events,", a.",nonevents,", ",var,") AS ",varname," FROM ",table_name," a, fzzlSerial b WHERE b.SerialVal <= ",n," GROUP BY b.SerialVal ORDER BY 1;")
+    return(vquery)
+}
+
+
+
+
+
 #' @export
 print.FLWOE <- function(object){
+    print(object@results$otable)
+}
 
-    return(object@results$otable)
+#' @export
+print.FLInfoVal <- function(object){
+    print(object@results$otable)
 }
 
 #' @export
 setMethod("show", signature("FLWOE"), function(object) {print.FLWOE(object)})
+
+#' @export
+setMethod("show", signature("FLInfoVal"), function(object) { print.FLInfoVal(object)})
+
+
+
+
+vdf <- data.frame(BinID = 1:4, vEvents =c(206,357,776,183) , vNonEvents = c(4615,9909,32150,12605))
+fltbl <- as.FL(vdf)
+
+
+
+
+
+
+"SELECT b.SerialVal AS BinID, FLWOE(a.BinID, a.vEvents, a.vNonEvents, b.SerialVal) AS WOE FROM ARBaseaW1499339697 a, fzzlSerial b WHERE b.SerialVal <= 4 GROUP BY b.SerialVal ORDER BY 1;"
+
+
+
+
+sqlQuery(connection, "SELECT b.SerialVal AS BinID,
+FLWOE(a.BinID, a.vEvents, a.vNonEvents, b.SerialVal) AS WOE
+FROM ARBaseaW1499339697 a,
+fzzlSerial b
+WHERE b.SerialVal <= 4
+GROUP BY b.SerialVal
+ORDER BY 1;")
+
+
